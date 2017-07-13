ac2d2a479c2874f3d194471779642e08541ec745
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Remove accidental definition of global #logger method"
    This reverts commit 2c60b114736d77503098085f4c79428db5e5bf06.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -12,10 +12,13 @@ Riiif::Image.info_service = lambda do |id, _file|
   { height: doc['height_is'], width: doc['width_is'] }
 end
 
+def logger
+  Rails.logger
+end
 
 Riiif::Image.file_resolver.id_to_uri = lambda do |id|
   ActiveFedora::Base.id_to_uri(CGI.unescape(id)).tap do |url|
-    Rails.logger.info "Riiif resolved #{id} to #{url}"
+    logger.info "Riiif resolved #{id} to #{url}"
   end
 end
 # Riiif::Image.file_resolver.basic_auth_credentials = [ActiveFedora.fedora.user, ActiveFedora.fedora.password]
