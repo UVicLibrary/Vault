4e0e346a68ca4703d8523968715ef826fd8033ae
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Configure docker to use a single redis node"
    This reverts commit cdc74025a8e3353e04dab89f3236d80f267ef0e2.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,23 @@
 config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
-redis_config = config.merge(thread_safe: true)
+sentinels = config[:sentinel] && config[:sentinel][:host].present? ? { sentinels: [config[:sentinel]] } : {}
+redis_config = config.except(:sentinel).merge(thread_safe: true).merge(sentinels)
+
+redis_conn = if ENV['DISABLE_REDIS_CLUSTER']
+               # Clustering must be disabled on AWS because it breaks Apartment switching.
+               # See https://github.com/projecthydra-labs/hyku/issues/430
+               proc { redis_config }
+             else
+               proc {
+                 ConnectionPool.new do
+                   Redis.new redis_config.merge(role: :master)
+                 end
+               }
+             end
 
 Sidekiq.configure_server do |s|
-  s.redis = redis_config
+  s.redis = redis_conn.call
 end
 
 Sidekiq.configure_client do |s|
-  s.redis = redis_config
+  s.redis = redis_conn.call
 end
