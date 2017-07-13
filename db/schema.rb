46ba1556775391233911444019e23cef5bca71d0
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Add new Hyrax migration for JobIoWrapper work"
    This reverts commit 6e9c4020ae51330d70a1234173e22f15afff0537.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712172828) do
+ActiveRecord::Schema.define(version: 20170605152346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,20 +151,6 @@ ActiveRecord::Schema.define(version: 20170712172828) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "job_io_wrappers", id: :serial, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "uploaded_file_id"
-    t.string "file_set_id"
-    t.string "mime_type"
-    t.string "original_name"
-    t.string "path"
-    t.string "relation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["uploaded_file_id"], name: "index_job_io_wrappers_on_uploaded_file_id"
-    t.index ["user_id"], name: "index_job_io_wrappers_on_user_id"
-  end
-
   create_table "local_authorities", id: :serial, force: :cascade do |t|
     t.string "name"
   end
@@ -178,8 +164,8 @@ ActiveRecord::Schema.define(version: 20170712172828) do
   end
 
   create_table "mailboxer_conversation_opt_outs", id: :serial, force: :cascade do |t|
-    t.string "unsubscriber_type"
     t.integer "unsubscriber_id"
+    t.string "unsubscriber_type"
     t.integer "conversation_id"
     t.index ["conversation_id"], name: "index_mailboxer_conversation_opt_outs_on_conversation_id"
     t.index ["unsubscriber_id", "unsubscriber_type"], name: "index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type"
@@ -195,13 +181,13 @@ ActiveRecord::Schema.define(version: 20170712172828) do
     t.string "type"
     t.text "body"
     t.string "subject", default: ""
-    t.string "sender_type"
     t.integer "sender_id"
+    t.string "sender_type"
     t.integer "conversation_id"
     t.boolean "draft", default: false
     t.string "notification_code"
-    t.string "notified_object_type"
     t.integer "notified_object_id"
+    t.string "notified_object_type"
     t.string "attachment"
     t.datetime "updated_at", null: false
     t.datetime "created_at", null: false
@@ -214,8 +200,8 @@ ActiveRecord::Schema.define(version: 20170712172828) do
   end
 
   create_table "mailboxer_receipts", id: :serial, force: :cascade do |t|
-    t.string "receiver_type"
     t.integer "receiver_id"
+    t.string "receiver_type"
     t.integer "notification_id", null: false
     t.boolean "is_read", default: false
     t.boolean "trashed", default: false
@@ -299,8 +285,8 @@ ActiveRecord::Schema.define(version: 20170712172828) do
 
   create_table "roles", id: :serial, force: :cascade do |t|
     t.string "name"
-    t.string "resource_type"
     t.integer "resource_id"
+    t.string "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
