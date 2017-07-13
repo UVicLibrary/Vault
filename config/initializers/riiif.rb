98877d597cc07577edb03e62375154889463ea32
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Update hyrax and other dependencies"
    This reverts commit 3dcf6dbd9401fff0ab68858f071770e1374b105d.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -27,6 +27,5 @@ end
 Riiif::Image.authorization_service = IIIFAuthorizationService
 
 Riiif.not_found_image = 'app/assets/images/us_404.svg'
-Riiif.unauthorized_image = 'app/assets/images/us_404.svg'
-
 Riiif::Engine.config.cache_duration_in_days = 365
+
