6f5d5385b1e00f3a5bf6d9166b39ba0855383b88
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Remove ContentBlock-related technical debt"
    This reverts commit 3fbe775709a025748a0d1fada927d6afd083b143.

--- app/controllers/content_blocks_controller.rb
+++ app/controllers/content_blocks_controller.rb
@@ -1,9 +1,9 @@
-class LabelsController < SitesController
+class ContentBlocksController < SitesController
   # GET /sites/1/edit
   def edit
     add_breadcrumb t(:'hyrax.controls.home'), root_path
     add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
-    add_breadcrumb t(:'hyrax.admin.sidebar.labels'), edit_site_labels_path
+    add_breadcrumb t(:'hyrax.admin.sidebar.content_blocks'), edit_site_content_blocks_path
   end
 
   # PATCH/PUT /sites/1
@@ -11,7 +11,7 @@ class LabelsController < SitesController
   def update
     respond_to do |format|
       if @site.update(site_params)
-        format.html { redirect_to edit_site_labels_path, notice: 'Labels were successfully updated.' }
+        format.html { redirect_to edit_site_content_blocks_path, notice: 'Site was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -22,8 +22,10 @@ class LabelsController < SitesController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def site_params
-      params.require(:site).permit(:application_name,
-                                   :institution_name,
-                                   :institution_name_full)
+      params.require(:site).permit(:announcement_text,
+                                   :marketing_text,
+                                   :featured_researcher,
+                                   :about_page,
+                                   :help_page)
     end
 end
