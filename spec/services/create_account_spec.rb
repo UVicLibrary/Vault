ef58688458c827cd55c02e7573c3f1d36e43c67b
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Fixes #1100; we need to monkey-patch ActiveJob::Base in order to tap into its deserialization behavior."
    This reverts commit f6678c1cfcda37071debf2bba0484bb2cdde93b0.

--- spec/services/create_account_spec.rb
+++ spec/services/create_account_spec.rb
@@ -1,5 +1,10 @@
 RSpec.describe CreateAccount do
-  let(:account) { FactoryGirl.build(:sign_up_account) }
+  # There are several "phases" of Account model in play here.
+  # (1) The basic proto-account from the sign_up page: basically just name.  See #save below.
+  # (2) The account *after* save, that has tenant and cname. This is what most methods here will see.
+  # (3) A switch-read account that is what we hope to produce by the end.
+
+  let(:account) { FactoryGirl.build(:sign_up_account, tenant: 'fresh_tenant', cname: 'fresh.localhost') }
   subject { described_class.new(account) }
 
   describe '#create_tenant' do
@@ -19,8 +24,14 @@ RSpec.describe CreateAccount do
   end
 
   describe '#create_account_inline' do
-    it 'runs account creation jobs' do
-      expect(CreateAccountInlineJob).to receive(:perform_now).with(account)
+    it 'updates endpoints' do
+      name = account.tenant.parameterize
+      expect(CreateSolrCollectionJob).to receive(:perform_now).with(subject.account)
+      expect(account).to receive(:update).with(
+        redis_endpoint_attributes: { namespace: name },
+        fcrepo_endpoint_attributes: { base_path: "/#{name}" }
+      )
+      expect(subject.account).to receive(:switch).never # Switch is not called here
       subject.create_account_inline
     end
   end
@@ -31,8 +42,8 @@ RSpec.describe CreateAccount do
     let(:account1) { CreateAccount.new(resource1) }
     let(:account2) { CreateAccount.new(resource2) }
     before do
-      allow(account1).to receive(:create_external_resources).and_return true
-      allow(account2).to receive(:create_external_resources).and_return true
+      allow(account1).to receive(:create_resources).and_return true
+      allow(account2).to receive(:create_resources).and_return true
     end
     it 'prevents duplicate accounts' do
       expect(account1.save).to be true
