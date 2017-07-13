19dd09eaa82595cde7d8fd5c0f3c21f499f990ea
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Remove duplicate include"
    This reverts commit 242db645c539beafd6c5ab4cf2197fb7eb3db21a.

--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -1,6 +1,7 @@
 class GenericWork < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::Hyrax::BasicMetadata
+  include Hyrax::WorkBehavior
   validates :title, presence: { message: 'Your work must have a title.' }
 
   # This indexer uses IIIF thumbnails:
