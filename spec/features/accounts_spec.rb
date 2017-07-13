b2c6b79d5d27998692c832954b318a858093dd56
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Refactor per comments and add specs"
    This reverts commit 99a604d6c851e49d70148cffdfc283dcadb2a05c.

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -9,15 +9,12 @@ RSpec.describe 'Accounts administration', multitenant: true do
     before do
       login_as(user, scope: :user)
       Capybara.default_host = "http://#{Account.admin_host}"
-      allow(Apartment::Tenant).to receive(:switch).with(account.tenant) do |&block|
-        block.call
-      end
     end
 
     it 'changes the associated cname' do
       visit edit_proprietor_account_path(account)
 
-      fill_in 'Tenant CNAME', with: 'example.com'
+      fill_in 'Cname', with: 'example.com'
 
       click_on 'Save'
 
