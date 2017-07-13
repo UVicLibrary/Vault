9f2d64e1f375d1fc3a6407f67e36748b3466ec22
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Tweak image model spec to reflect change to indexer"
    This reverts commit 9f60e299ea4b6094ff4af440275651407bdfd5ff.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,8 +2,7 @@
 #  `rails generate hyrax:work Image`
 
 RSpec.describe Image do
-  describe 'indexer' do
-    subject { described_class.indexer }
-    it { is_expected.to eq ImageIndexer }
+  it "has tests" do
+    skip "Add your tests here"
   end
 end
