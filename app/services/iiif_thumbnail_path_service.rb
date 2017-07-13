201a5f773dbf28cd5bb8dc701a2a1ac97be50912
Braydon Justice <sephirothkod@users.noreply.github.com>
2017-07-13 13:36:29 -0700

    Revert "Only make iiif thumbnails for images."
    This reverts commit 7f7c9e02aaa1f28b331bb85d4058fa9384259627.

--- app/services/iiif_thumbnail_path_service.rb
+++ app/services/iiif_thumbnail_path_service.rb
@@ -5,15 +5,8 @@ class IIIFThumbnailPathService < Hyrax::WorkThumbnailPathService
       # @param [FileSet] file_set
       # @param [String] size ('!150,300') an IIIF image size defaults to an image no
       #                      wider than 150px and no taller than 300px
-      # @return the IIIF url for the thumbnail if it's an image, otherwise gives
-      #         the thumbnail download path
+      # @return the IIIF url for the thumbnail.
       def thumbnail_path(file_set, size = '!150,300'.freeze)
-        return super(file_set) unless file_set.image?
-        iiif_thumbnail_path(file_set, size)
-      end
-
-      # @private
-      def iiif_thumbnail_path(file_set, size)
         file = file_set.original_file
         return unless file
         Riiif::Engine.routes.url_helpers.image_path(
@@ -26,5 +19,4 @@ class IIIFThumbnailPathService < Hyrax::WorkThumbnailPathService
         true
       end
   end
-  private_class_method :iiif_thumbnail_path
 end
