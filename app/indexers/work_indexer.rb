15a9806f59592fdafd7bac8603786a98e4bd389a
Tiffany Chan <tjychan@uvic.ca>
2020-01-10 10:55:34 -0800

    Added title sort and year sort fields to work indexer. Configured those fields to show up in searches, works list (on collection public pages), etc.

--- app/indexers/work_indexer.rb
+++ app/indexers/work_indexer.rb
@@ -14,7 +14,40 @@ class WorkIndexer < Hyrax::WorkIndexer
    def generate_solr_document
     super.tap do |solr_doc|
       solr_doc['title_sort_tesi'] = object.title.first unless object.title.first.nil?
-      solr_doc['year_sort_tesi'] = object.year.first unless object.year.first.nil?
+
+      if solr_doc['date_created_tesim']
+        date = Date.edtf(solr_doc['date_created_tesim'].first)
+        if date.class == EDTF::Interval
+          solr_doc['year_sort_dtsim'] = solrize(date)
+          solr_doc['year_sort_dtsi'] = solrize(date).first
+        else # date.class == Date
+          solr_doc['year_sort_dtsim'] = solr_string(date)
+          solr_doc['year_sort_dtsi'] = solr_string(date)
+        end
+      end
     end
-   end
+  end
+
+  # Returns an array of solr_stringified dates
+  def solrize(edtf_date)
+    # Return an array of all years included in the interval
+    years = edtf_date.map { |d| d.year.to_s }.uniq # Without .uniq, 1981-02/1981-03 would return ["1981", "1981"]
+    dates_array = years.map { |y| Date.edtf(y) }     # Array of edtf date objects
+    # If the start or end dates of the interval have day or month precision, keep the month/day
+    if edtf_date.from.day_precision? or edtf_date.from.month_precision?
+      dates_array[0] = edtf_date.from
+    end
+    if edtf_date.to.day_precision? or edtf_date.to.month_precision?
+      dates_array[-1] = edtf_date.to unless dates_array.count == 1 # Don't overwrite the from date if from & to have the same year
+    end
+    dates_array.map{ |d| solr_string(d) }
+  end
+
+  # Returns formatted string with time set to midnight; e.g. Wed, 01 Jan 1913 => "1913-01-01T00:00:00Z"
+  # https://lucene.apache.org/solr/guide/7_7/working-with-dates.html
+  def solr_string(edtf_date)
+    date_time = edtf_date.beginning_of_day.to_s.split(" ") - ["UTC"] # => ["1913-01-01", "00:00:00"]
+    "#{date_time[0]}T#{date_time[1]}Z"
+  end
+
 end
