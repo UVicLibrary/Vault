cad4174ae0f588132b45e57fb0796322d5fb7ec4
Tiffany Chan <tjychan@uvic.ca>
2020-06-24 12:38:25 -0700

    Update file_set_indexer.rb

--- app/indexers/file_set_indexer.rb
+++ app/indexers/file_set_indexer.rb
@@ -4,6 +4,10 @@ class FileSetIndexer < Hyrax::FileSetIndexer
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc['hasFormat_ssim'] = object.rendering_ids
+      if object.creator.present?
+        solr_doc['creator_tesim'] = Array.wrap(object.creator.first.id) 
+        solr_doc['creator_label_tesim'] = object.creator.first.rdf_label
+      end
     end
   end
 end
