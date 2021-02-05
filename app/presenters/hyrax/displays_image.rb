a43c2bc3d00005d9a993c6a1b30c75df287efb32
Tiffany Chan <tiff.chan29@gmail.com>
2021-02-05 15:57:31 -0800

    (Hopefully) fix error where UV and Riiif display earlier versions of an image even after a new version is uploaded/restored.
    Also fix minor display issues.

---
+++ app/presenters/hyrax/displays_image.rb
@@ -0,0 +1,48 @@
+require 'iiif_manifest'
+
+module Hyrax
+  # This gets mixed into FileSetPresenter in order to create
+  # a canvas on a IIIF manifest
+  module DisplaysImage
+    extend ActiveSupport::Concern
+
+    # Creates a display image only where FileSet is an image.
+    #
+    # @return [IIIFManifest::DisplayImage] the display image required by the manifest builder.
+    # Fix error where Riiif holds onto earlier versions of an image
+    # See https://github.com/samvera/hyrax/pull/3165
+    def display_image
+      return nil unless ::FileSet.exists?(id) && solr_document.image? && current_ability.can?(:read, id)
+      # @todo this is slow, find a better way (perhaps index iiif url):
+      # original_file = ::FileSet.find(id).original_file
+      # latest version = ::FileSet.find(id).files.first.versions.last.uri
+      latest_version_id = ActiveFedora::Base.uri_to_id(::FileSet.find(id).latest_content_version.uri)
+
+      url = Hyrax.config.iiif_image_url_builder.call(
+          latest_version_id, # original_file.id,
+          request.base_url,
+          Hyrax.config.iiif_image_size_default, ""
+      )
+      # Passing empty string suppresses the "wrong number of arguments" error.
+      # Hyrax.config says the last argument is _format but not what that's supposed to be.
+
+      # @see https://github.com/samvera-labs/iiif_manifest
+      IIIFManifest::DisplayImage.new(url,
+                                     width: 640,
+                                     height: 480,
+                                     iiif_endpoint: iiif_endpoint(latest_version_id)) # iiif_endpoint(original_file.id)
+
+
+    end
+
+    private
+
+    def iiif_endpoint(file_id)
+      return unless Hyrax.config.iiif_image_server?
+      IIIFManifest::IIIFEndpoint.new(
+          Hyrax.config.iiif_info_url_builder.call(file_id, request.base_url),
+          profile: Hyrax.config.iiif_image_compliance_level_uri
+      )
+    end
+  end
+end
