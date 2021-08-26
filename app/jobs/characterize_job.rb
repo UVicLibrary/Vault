6beadaf361ebc3d5719e81b84156e4012ab470ef
Tiffany Chan <tjychan@uvic.ca>
2021-08-26 12:34:03 -0700

    Bump Sidekiq to v. 6

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -7,16 +7,16 @@ class CharacterizeJob < Hyrax::ApplicationJob
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
   def perform(file_set, file_id, filepath = nil)
-    Sidekiq::Logging.logger.info "Hit CharacterizeJob for #{file_set.id}"
+    # Sidekiq::JobLogger.info "Hit CharacterizeJob for #{file_set.id}"
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
     filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
     Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
-    Sidekiq::Logging.logger.info "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
+    # Sidekiq::JobLogger.info "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
     file_set.characterization_proxy.save!
     file_set.update_index
-    Sidekiq::Logging.logger.info "Updated and indexed #{file_set.id}"
+    # Sidekiq::JobLogger.info "Updated and indexed #{file_set.id}"
     file_set.parent&.in_collections&.each(&:update_index)
-    Sidekiq::Logging.logger.info "Updated and indexed #{file_set.parent&.in_collections&.each(&:update_index)}"
+    # Sidekiq::JobLogger.info "Updated and indexed #{file_set.parent&.in_collections&.each(&:update_index)}"
     CreateDerivativesJob.perform_later(file_set, file_id, filepath)
   end
 end
