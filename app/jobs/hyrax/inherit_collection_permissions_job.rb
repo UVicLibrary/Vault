80e42065b94079c48237dffa7abd5eabb4d50b6e
Tiffany Chan <tjychan@uvic.ca>
2021-09-29 15:09:18 -0700

    Tweak views for displaying collection and work permissions. Save works and file sets so that permission attributes persist!

--- app/jobs/hyrax/inherit_collection_permissions_job.rb
+++ app/jobs/hyrax/inherit_collection_permissions_job.rb
@@ -5,14 +5,17 @@ module Hyrax
       coll = Collection.find(collection_id)
       works = GenericWork.where(member_of_collection_ids_ssim: collection_id)
       coll_permissions = coll.permissions.map(&:to_hash)
-      # Copy collection permissions to works
-      works.each do |work|
-        # We overwrite work's permissions with collection permissions.
-        work.permissions_attributes = coll_permissions
-        work.file_sets.each do |file_set|
-          file_set.permissions_attributes = coll_permissions
+        # Copy collection permissions to works
+        works.each do |work|
+          # We overwrite work's permissions with collection permissions.
+          work.permissions_attributes = coll_permissions
+          work.save!
+          work.file_sets.each do |file_set|
+            file_set.permissions_attributes = coll_permissions
+            file_set.save!
+          end
         end
-      end
+
       # Send an email when done
       ::VisibilityPermissionsMailer.with(account_host: host, user_email: user_email, id: collection_id).inherit_permissions.deliver
     end
