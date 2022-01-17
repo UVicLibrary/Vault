3577989beccae447aaa79db77ab4f09db362d9e5
eltiffster <tjychan@uvic.ca>
2022-01-17 10:09:08 -0800

    Update EdtfDateService to parse century and decade intervals (e.g. 14XX/15XX, 198X/199X).

--- app/services/edtf_date_service.rb
+++ app/services/edtf_date_service.rb
@@ -18,134 +18,166 @@ class EdtfDateService
 
   require 'edtf'
 
-    def initialize(edtf_string)
-      @edtf_string = edtf_string
-      @parsed_date = parse_date(edtf_string)
-    end
+  def initialize(edtf_string)
+    @edtf_string = edtf_string
+    @parsed_date = parse_date(edtf_string)
+  end
 
-    # Return date in solr format
-    def solr_date_range
-      if ([EDTF::Interval, EDTF::Decade, EDTF::Century, EDTF::Season].include?(@parsed_date.class)) or season_interval?(@edtf_string)
-        @parsed_date.map{|d| d.strftime("%FT%TZ")}
-      elsif @parsed_date.class == Date
-        Array.wrap(@parsed_date.strftime("%FT%TZ"))
-      end
+  # Return date in solr format
+  def solr_date_range
+    if ([EDTF::Interval, EDTF::Decade, EDTF::Century, EDTF::Season].include?(@parsed_date.class)) or season_interval?(@edtf_string)
+      @parsed_date.map{|d| d.strftime("%FT%TZ")}
+    elsif @parsed_date.class == Date
+      Array.wrap(@parsed_date.strftime("%FT%TZ"))
     end
+  end
 
-    # Returns first solr date
-    def first_solr_date
-      solr_date_range.first if solr_date_range
+  # Returns first solr date
+  def first_solr_date
+    solr_date_range.first if solr_date_range
+  end
+
+  def year_range
+    case @parsed_date.class.name
+    when "Date"
+      Array.wrap(@parsed_date.year.to_i)
+    when "String"
+      nil
+    else
+      @parsed_date.map { |d| d.year.to_i }.uniq
     end
+  end
 
-    def year_range
-      case @parsed_date.class.name
-      when "Date"
-        Array.wrap(@parsed_date.year.to_i)
-      when "String"
-        nil
-      else
-        @parsed_date.map { |d| d.year.to_i }.uniq
-      end
-    end
+  def first_year
+    year_range.first if year_range
+  end
 
-    def first_year
-      year_range.first if year_range
-    end
-
-    # Returns human-readable date (string). See edtf-humanize:
-    # https://github.com/duke-libraries/edtf-humanize
-    def humanized
-      case @parsed_date.class.name
-      when "EDTF::Season"
-        humanized_season(@parsed_date)
-      when "EDTF::Interval"
-        if season_interval?(@edtf_string)
-          humanized_season_interval(@edtf_string)
-        elsif approx_and_uncertain?(@edtf_string)
-          humanized_approx_and_uncertain_interval
-        else
-          @parsed_date.humanize.gsub('circa','approximately')
-        end
-      when "String" # "unknown" or "no date"
-        @parsed_date
-      when "Date"
-        if approx_and_uncertain?(@edtf_string)
-          humanized_approx_and_uncertain_date(@parsed_date.humanize)
-        else
-          @parsed_date.humanize.gsub('circa','approximately')
-        end
+  # Returns human-readable date (string). See edtf-humanize:
+  # https://github.com/duke-libraries/edtf-humanize
+  def humanized
+    case @parsed_date.class.name
+    when "EDTF::Season"
+      humanized_season(@parsed_date)
+    when "EDTF::Interval"
+      if season_interval?(@edtf_string)
+        humanized_season_interval(@edtf_string)
+      elsif century_or_decade_interval?(@edtf_string)
+        humanized_century_or_decade_interval(@edtf_string)
+      elsif approx_and_uncertain?(@edtf_string)
+        humanized_approx_and_uncertain_interval
       else
         @parsed_date.humanize.gsub('circa','approximately')
       end
+    when "String" # "unknown" or "no date"
+      @parsed_date
+    when "Date"
+      if approx_and_uncertain?(@edtf_string)
+        humanized_approx_and_uncertain_date(@parsed_date.humanize)
+      else
+        @parsed_date.humanize.gsub('circa','approximately')
+      end
+    else
+      @parsed_date.humanize.gsub('circa','approximately')
     end
+  end
 
-    class InvalidEdtfDateError < StandardError; end
+  class InvalidEdtfDateError < StandardError; end
 
-    def self.error_classes
-      [InvalidEdtfDateError]
+  def self.error_classes
+    [InvalidEdtfDateError]
+  end
+
+  private
+
+  def approx_and_uncertain?(date_string)
+    date_string.include?('%')
+  end
+
+  def humanized_approx_and_uncertain_date(parsed_date)
+    parsed_date.gsub('circa','approximately') << '?'
+  end
+
+  def humanized_approx_and_uncertain_interval
+    strings = @parsed_date.humanize.split("to ").map do |string|
+      string = string.strip
+      humanized_approx_and_uncertain_date(string)
     end
+    strings.join(" to ")
+  end
 
-    private
+  def season?(date_string)
+    Date.edtf(date_string).class == EDTF::Season
+  end
 
-      def approx_and_uncertain?(date_string)
-        date_string.include?('%')
+  def century?(date_string)
+    Date.edtf(date_string).class == EDTF::Century
+  end
+
+  def decade?(date_string)
+    Date.edtf(date_string).class == EDTF::Decade
+  end
+
+  def season_interval?(date_string)
+    season?(date_string.split("/").first) and season?(date_string.split("/").second)
+  end
+
+  def century_or_decade_interval?(date_string)
+    first_date = date_string.split("/").first
+    last_date = date_string.split("/").second
+    (century?(first_date) and century?(last_date)) ||
+        (decade?(first_date) and decade?(last_date))
+  end
+
+  def century_interval?(date_string)
+    century?(date_string.split("/").first) and century?(date_string.split("/").second)
+  end
+
+  def decade_interval?(date_string)
+    decade?(date_string.split("/").first) and decade?(date_string.split("/").second)
+  end
+
+  def parse_date(date_string)
+    date_string = date_string.gsub('%','~')
+    if date_string == "unknown" or date_string == "no date"
+      date_string
+    elsif season_interval?(date_string) or century_or_decade_interval?(date_string)
+      # edtf can't normally parse season, century, or decade intervals,
+      # so we create an interval using the first season's first date
+      # and the last season's last date
+      first_date = Date.edtf(date_string.split("/").first)
+      last_date = Date.edtf(date_string.split("/").last)
+      EDTF::Interval.new(first_date.first, last_date.last)
+    elsif Date.edtf(date_string).nil? # Invalid date
+      if date_string.include? "#"
+        raise InvalidEdtfDateError.new("Could not parse date: \"#{date_string}.\" Date includes #, please use X or another alternative.")
+      else
+        raise InvalidEdtfDateError.new("Could not parse date: \"#{date_string}.\" If date is unknown, use \"unknown\" or \"no date\"")
       end
-
-      def humanized_approx_and_uncertain_date(parsed_date)
-        parsed_date.gsub('circa','approximately') << '?'
+    else
+      # modify date so that the interval encompasses the years on the last interval date
+      temp_date = date_string.gsub('/..','').gsub('%','?').gsub(/\/$/,'')
+      date = temp_date.include?("/") ? temp_date.gsub(/([0-9]+X+\/)([0-9]+)(X+)/){"#{$1}"+"#{$2.to_i+1}"+"#{$3}"}.gsub("X","u") : temp_date
+      if match = date[/\d{3}u/] # edtf can't parse single u in year (e.g. 192u), so we replace it
+        date.gsub!(match, match.gsub("u","0"))
       end
+      Date.edtf(date)
+    end
+  end
 
-      def humanized_approx_and_uncertain_interval
-        strings = @parsed_date.humanize.split("to ").map do |string|
-          string = string.strip
-          humanized_approx_and_uncertain_date(string)
-        end
-        strings.join(" to ")
-      end
+  def humanized_season(season)
+    season.humanize.split(" ").map(&:capitalize).join(", ")
+  end
 
-      def season?(date_string)
-        Date.edtf(date_string).class == EDTF::Season
-      end
+  def humanized_season_interval(date_string)
+    first_season = Date.edtf(date_string.split("/").first)
+    last_season = Date.edtf(date_string.split("/").last)
+    "#{humanized_season(first_season)} to #{humanized_season(last_season)}"
+  end
 
-      def season_interval?(date_string)
-        season?(date_string.split("/").first) and season?(date_string.split("/").second)
-      end
-
-      def parse_date(date_string)
-        date_string = date_string.gsub('%','~')
-        if date_string == "unknown" or date_string == "no date"
-          date_string
-        elsif season_interval?(date_string)
-          first_season = Date.edtf(date_string.split("/").first)
-          last_season = Date.edtf(date_string.split("/").last)
-          # edtf can't parse season intervals, so we create an interval using the first season's
-          # first date and the last season's last date
-          EDTF::Interval.new(first_season.first, last_season.last)
-        elsif Date.edtf(date_string).nil? # Invalid date
-          if date_string.include? "#"
-            raise InvalidEdtfDateError.new("Could not parse date: \"#{date_string}.\" Date includes #, please use X or another alternative.")
-          else
-            raise InvalidEdtfDateError.new("Could not parse date: \"#{date_string}.\" If date is unknown, use \"unknown\" or \"no date\"")
-          end
-        else
-          # modify date so that the interval encompasses the years on the last interval date
-          temp_date = date_string.gsub('/..','').gsub('%','?').gsub(/\/$/,'')
-          date = temp_date.include?("/") ? temp_date.gsub(/([0-9]+X+\/)([0-9]+)(X+)/){"#{$1}"+"#{$2.to_i+1}"+"#{$3}"}.gsub("X","u") : temp_date
-          if match = date[/\d{3}u/] # edtf can't parse single u in year (e.g. 192u), so we replace it
-            date.gsub!(match, match.gsub("u","0"))
-          end
-          Date.edtf(date)
-        end
-      end
-
-      def humanized_season(season)
-        season.humanize.split(" ").map(&:capitalize).join(", ")
-      end
-
-      def humanized_season_interval(date_string)
-        first_season = Date.edtf(date_string.split("/").first)
-        last_season = Date.edtf(date_string.split("/").last)
-        "#{humanized_season(first_season)} to #{humanized_season(last_season)}"
-      end
+  def humanized_century_or_decade_interval(date_string)
+    first_date = Date.edtf(date_string.split("/").first)
+    last_date = Date.edtf(date_string.split("/").last)
+    "#{first_date.humanize} to #{last_date.humanize}"
+  end
 
 end
