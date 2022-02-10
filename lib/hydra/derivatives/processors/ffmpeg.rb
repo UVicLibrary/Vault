5e460ba358d1a83ce319fa6a54b0af081586f1c3
eltiffster <tjychan@uvic.ca>
2022-02-10 10:02:13 -0800

    Fix bug with audio thumbnails. Processor should only return on an m4a file and not other audio formats.

--- lib/hydra/derivatives/processors/ffmpeg.rb
+++ lib/hydra/derivatives/processors/ffmpeg.rb
@@ -17,7 +17,7 @@ module Hydra::Derivatives::Processors
 
         # Do nothing for m4a files, which hydra works interprets as 'video/mp4' mime type
         # ffprobe returns the codec_type, which is audio for m4a
-        return if `ffprobe -loglevel error -show_entries stream=codec_type -of csv=p=0 #{path}` == "audio\n"
+        return if (`ffprobe -loglevel error -show_entries stream=codec_type -of csv=p=0 #{path}` == "audio\n") && path.include?(".m4a")
 
         execute "#{Hydra::Derivatives.ffmpeg_path} #{inopts.gsub('INPUT_PATH', Shellwords.escape(path))} -i #{Shellwords.escape(path)} #{outopts} #{output_file}"
       end
