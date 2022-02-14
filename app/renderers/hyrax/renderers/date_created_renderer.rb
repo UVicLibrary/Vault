d16ae15ad430491173a04a2e6091abc05cd42836
eltiffster <tjychan@uvic.ca>
2022-02-14 11:41:17 -0800

    Render date_created dates in chronological/alphabetical order.

--- app/renderers/hyrax/renderers/date_created_renderer.rb
+++ app/renderers/hyrax/renderers/date_created_renderer.rb
@@ -2,7 +2,19 @@ module Hyrax
   module Renderers
     class DateCreatedRenderer < AttributeRenderer
 
-      # Converted into a year range rather than a text search.
+      # Draw the table row for the attribute. Render values in chronological/alphabetical order
+      def render
+        markup = ''
+
+        return markup if values.blank? && !options[:include_empty]
+        markup << %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
+        attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
+        Array(values.sort).each do |value|
+          markup << "<li#{html_attributes(attributes)}>#{attribute_value_to_html(value.to_s)}</li>"
+        end
+        markup << %(</ul></td></tr>)
+        markup.html_safe
+      end
 
     private
 
@@ -10,6 +22,7 @@ module Hyrax
         link_to(value, search_path)
       end
 
+      # Converted into a year range rather than a text search.
       def search_path
         if options.fetch(:begin).present? and options.fetch(:end).present?
           Rails.application.routes.url_helpers.search_catalog_path + range_params_string(options.fetch(:begin), options.fetch(:end))
