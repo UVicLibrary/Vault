6b80c889f5fcafb8bd5d3dbd0dc250e90ae2e985
Tiffany Chan <tjychan@uvic.ca>
2022-02-22 10:02:21 -0800

    Add pdf thumbnail path service (to work indexer).

---
+++ app/services/pdf_thumbnail_path_service.rb
@@ -0,0 +1,39 @@
+class PdfThumbnailPathService < Hyrax::WorkThumbnailPathService
+  # We use poppler to generate pdf thumbnails instead of ImageMagick/MiniMagick.
+  # (See file set derivatives service.) Index that new path into the thumbnail
+  # field for works.
+
+  class << self
+
+    # @param [file set] object - to get the thumbnail for
+    # @return [String] a path to the thumbnail
+    def call(object)
+      return default_image unless object.thumbnail_id
+      collection = object.parent.member_of_collections
+      collection_name = object.parent.member_of_collections.first.title.first.parameterize.underscore if collection.any?
+      if collection_name && File.exist?("#{public_path}#{pdf_path(collection_name, object.id)}")
+        pdf_path(collection_name, object.id)
+      elsif File.exist?("#{public_path}#{pdf_path(collection_name, object.id)}")
+        misc_path(object.id)
+      else
+        default_image
+      end
+    end
+
+  private
+
+    def pdf_path(collection_title, file_set_id)
+      "/pdf_thumbnails/#{collection_title}/#{file_set_id}-thumb.jpg"
+    end
+
+    def misc_path(file_set_id)
+      "/pdf_thumbnails/misc/#{file_set_id}-thumb.jpg"
+    end
+
+    # Path to public folder
+    def public_path
+      "#{Rails.root.to_s}/public"
+    end
+
+  end
+end
