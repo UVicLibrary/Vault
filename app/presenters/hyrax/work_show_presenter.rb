3ce42bd12a010e271283c8c3b48d868da77bbb70
Tiffany Chan <tjychan@uvic.ca>
2022-03-01 11:01:22 -0800

    Delete thumbnail code from views/partials.

--- app/presenters/hyrax/work_show_presenter.rb
+++ app/presenters/hyrax/work_show_presenter.rb
@@ -18,7 +18,7 @@ module Hyrax
     				 :contributor_label, :physical_repository_label, :genre_label,
     				 :geographic_coverage, :genre, :related_url, :depositor, :identifier,
     				 :resource_type, :keyword, :itemtype, :admin_set, :geographic_coverage_label,
-             :chronological_coverage,
+             :chronological_coverage, :thumbnail_path,
              to: :solr_document
 
     # @param [SolrDocument] solr_document
