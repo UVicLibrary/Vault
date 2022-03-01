8169c5458958cb4da0efcd009032a00a1289de58
Tiffany Chan <tjychan@uvic.ca>
2022-03-01 11:45:41 -0800

    Index larger thumbnails for collections.

---
+++ app/services/concerns/large_iiif_thumbnail_paths.rb
@@ -0,0 +1,16 @@
+module LargeIIIFThumbnailPaths
+  extend ActiveSupport::Concern
+
+  class_methods do
+    # @param [FileSet] file_set
+    # @param [String] size ('!500,900') an IIIF image size defaults to an image no
+    #                      wider than 150px and no taller than 300px
+    # @return the IIIF url for the thumbnail if it's an image, otherwise gives
+    #         the thumbnail download path
+    def thumbnail_path(file_set, size = '!500,900'.freeze)
+      return super(file_set) unless file_set.image?
+      iiif_thumbnail_path(file_set, '!500,900')
+    end
+  end
+
+end
--- app/services/iiif_collection_thumbnail_path_service.rb
+++ app/services/iiif_collection_thumbnail_path_service.rb
@@ -1,5 +1,6 @@
 class IIIFCollectionThumbnailPathService < Hyrax::CollectionThumbnailPathService
   include IIIFThumbnailPaths
+  include LargeIIIFThumbnailPaths
 
   # The image to use if no thumbnail has been selected
   def self.default_image
