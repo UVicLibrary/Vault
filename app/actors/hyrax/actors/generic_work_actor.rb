bd4be446bafa1bf8fe18f7e0189c40f52fbad8b6
Tiffany Chan <tjychan@uvic.ca>
2022-04-28 11:08:59 -0700

    Explicitly add admins to edit groups for newly created private works.
    Previously, the "total viewable items" didn't include admins in the edit group even if admins could already edit private works deposited by others. This caused a mismatch between that and the true number of viewable items. This includes admins explicitly if a work is set to private so that the numbers match.

--- app/actors/hyrax/actors/generic_work_actor.rb
+++ app/actors/hyrax/actors/generic_work_actor.rb
@@ -3,23 +3,34 @@
 module Hyrax
   module Actors
     class GenericWorkActor < Hyrax::Actors::BaseActor
-    	
+
     	def update(env)
         apply_update_data_to_curation_concern(env)
         apply_save_data_to_curation_concern(env)
         next_actor.update(env) && save(env) && run_callbacks(:after_update_metadata, env)
       end
-      
+
       def apply_save_data_to_curation_concern(env)
-				cleaned_attributes = clean_attributes(env.attributes)
-      	env.curation_concern.attributes = clean_controlled_properties(env, cleaned_attributes)
-				env.curation_concern.date_modified = TimeService.time_in_utc
-			end
-      
-			def clean_controlled_properties(env, attributes)
-				qa_attributes = {}
-				env.curation_concern.controlled_properties.each do |field_symbol|
-					field = field_symbol.to_s
+        cleaned_attributes = clean_attributes(env.attributes)
+        env.curation_concern.attributes = clean_controlled_properties(env, cleaned_attributes)
+        env.curation_concern.date_modified = TimeService.time_in_utc
+      end
+
+      def apply_creation_data_to_curation_concern(env)
+        # Previously, the "total viewable items" didn't include admins in the edit group
+        # even if admins could already edit private works deposited by others.
+        # This caused a mismatch between that and the true number of viewable items. This
+        # includes admins explicitly if a work is set to private so that the numbers match.
+        if env.curation_concern.visibility == "restricted"
+          env.curation_concern.edit_groups = (env.curation_concern.edit_groups += ["admin"])
+        end
+        super
+      end
+
+      def clean_controlled_properties(env, attributes)
+        qa_attributes = {}
+        env.curation_concern.controlled_properties.each do |field_symbol|
+          field = field_symbol.to_s
           # Do not include deleted attributes
 					next unless attributes.keys.include?(field+'_attributes')
           filtered_attributes = attributes[field+'_attributes'].select  { |k,v| v['_destroy'].blank? }
@@ -32,7 +43,7 @@ module Hyrax
         save(env)
 				attributes
 			end
-      
+
     end
   end
 end
