1ab77a167d9955d7c746d800c2a99681124ce3df
Tiffany Chan <tjychan@uvic.ca>
2022-06-13 17:01:12 -0700

    Fix factory bot duplication error.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,5 +1,7 @@
 # RSpec
 # spec/support/factory_bot.rb
+# Comment this out after running spec tests once; Otherwise,
+# we get a duplicate definitions error.
 require 'factory_bot'
 
 RSpec.configure do |config|
@@ -10,6 +12,8 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
+  # Comment this out after running spec tests once; Otherwise,
+  # we get a duplicate definitions error.
   config.before(:suite) do
     FactoryBot.find_definitions
   end
