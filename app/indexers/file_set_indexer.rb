c2e98a8123cd51298ac3d7c85ba7a8436c299e00
Tiffany Chan <tjychan@uvic.ca>
2022-08-18 14:45:23 -0700

    Update file set indexer.

--- app/indexers/file_set_indexer.rb
+++ app/indexers/file_set_indexer.rb
@@ -1,6 +1,6 @@
 class FileSetIndexer < Hyrax::FileSetIndexer
   include Hyrax::IndexesLinkedMetadata
-  self.thumbnail_path_service = IIIFWorkThumbnailPathService
+  self.thumbnail_path_service = VaultThumbnailPathService
 
   def generate_solr_document
     # Convert ActiveTriples::Resource to Hyrax::ControlledVocabulary::[field name]
@@ -13,21 +13,12 @@ class FileSetIndexer < Hyrax::FileSetIndexer
 
     super.tap do |solr_doc|
       solr_doc['hasFormat_ssim'] = object.rendering_ids
-      if object.files.first.present?
-        if object.audio?  or (object.files.first.file_name.first.include?(".m4a") if object.files.first.file_name.first.present?)
-          solr_doc['thumbnail_path_ss'] = AudioFileSetThumbnailService.call(object)
-        else
-          solr_doc['thumbnail_path_ss'] = self.thumbnail_path_service.call(object)
-        end
-      end
-
+      # File sets should inherit the creators of their parents. Otherwise, the default "creator"
+      # is indexed as the depositor instead of the work's creator.
       if object.parent and object.parent.creator.present?
         parent_doc = SolrDocument.find(object.parent.id)
         solr_doc['creator_tesim'] = parent_doc['creator_tesim']
         solr_doc['creator_label_tesim'] = parent_doc['creator_label_tesim']
-      end
-      if object.pdf?
-        solr_doc['thumbnail_path_ss'] = PdfThumbnailPathService.call(object)
       end
     end
   end
