9a510deecf38006928e96ef8e3462d720315168b
Ethan Getty <35879251+EGetty@users.noreply.github.com>
2023-01-04 16:02:57 -0800

    Add pop-up warning user of unsaved changes

---
+++ app/assets/javascripts/hyrax/nav_safety.js
@@ -0,0 +1,44 @@
+// To enable nav safety on a form:
+// - Render the shared/nav-safety partial on the page.
+// - Add the nav-safety-confirm class to the tab anchor element.
+// - Add the nav-safety class to the form element.
+
+Blacklight.onLoad(function() {
+    var clickedTab;
+    $('.nav-safety-confirm').on('click', function(evt) {
+        debugger
+        clickedTab = $(this).attr('href');
+        var dirtyData = $('#nav-safety-modal[dirtyData=true]');
+        if (dirtyData.length > 0) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            $('#nav-safety-modal').modal('show');
+        }
+    });
+
+    $('#nav-safety-dismiss').on('click', function(evt) {
+        nav_safety_off();
+        // Navigate away from active tab to clicked tab
+        window.location = clickedTab;
+    });
+
+    $('form.nav-safety').on('change', function(evt) {
+        nav_safety_on();
+    });
+    $('form.nav-safety').on('reset', function(evt) {
+        nav_safety_off();
+    });
+});
+
+function nav_safety_on() {
+    $('#nav-safety-modal')[0].setAttribute('dirtyData', true);
+}
+
+function nav_safety_off() {
+    $('#nav-safety-modal')[0].setAttribute('dirtyData', false);
+}
+
+function tinymce_nav_safety(editor) {editor.on('Change', function (e) {
+    $(e.target.targetElm).parents('form.nav-safety').change();
+});
+}
