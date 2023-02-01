7bcf63cec68d51bd8406478b7dfedffa3606a21d
Tiffany Chan <tjychan@uvic.ca>
2023-02-01 10:29:27 -0800

    Update collection_behavior.rb to fix issue where legacy collection types don't appear in the interface.
    Then run this rake task: https://github.com/samvera/hyrax/blob/main/lib/tasks/collection_type_global_id.rake

--- app/models/concerns/hyrax/collection_behavior.rb
+++ app/models/concerns/hyrax/collection_behavior.rb
@@ -29,16 +29,17 @@ module Hyrax
       validates :collection_type_gid, presence: true
 
       # Need to define here in order to override setter defined by ActiveTriples
-      def collection_type_gid=(new_collection_type_gid)
-        raise "Can't modify collection type of this collection" if persisted? && !collection_type_gid_was.nil? && collection_type_gid_was != new_collection_type_gid
+      def collection_type_gid=(new_collection_type_gid, force: false)
+        new_collection_type_gid = new_collection_type_gid&.to_s
+        raise "Can't modify collection type of this collection" if !force && persisted? && !collection_type_gid_was.nil? && collection_type_gid_was != new_collection_type_gid
         new_collection_type = Hyrax::CollectionType.find_by_gid!(new_collection_type_gid)
-        super
+        super(new_collection_type_gid)
         @collection_type = new_collection_type
         collection_type_gid
       end
     end
 
-    delegate(*Hyrax::CollectionType.collection_type_settings_methods, to: :collection_type)
+    delegate(*Hyrax::CollectionType.settings_attributes, to: :collection_type)
 
     # Get the collection_type when accessed
     def collection_type
@@ -46,19 +47,22 @@ module Hyrax
     end
 
     def collection_type=(new_collection_type)
-      self.collection_type_gid = new_collection_type.gid
+      self.collection_type_gid = new_collection_type.to_global_id
     end
 
     # Add members using the members association.
+    # TODO: Confirm if this is ever used.  I believe all relationships are done through
+    #       add_member_objects using the member_of_collections relationship.  Deprecate?
     def add_members(new_member_ids)
       return if new_member_ids.blank?
-      members << ActiveFedora::Base.find(new_member_ids)
+      members << Hyrax.custom_queries.find_many_by_alternate_ids(alternate_ids: new_member_ids, use_valkyrie: false)
     end
 
     # Add member objects by adding this collection to the objects' member_of_collection association.
+    # @param [Enumerable<String>] the ids of the new child collections and works collection ids
     def add_member_objects(new_member_ids)
       Array(new_member_ids).collect do |member_id|
-        member = ActiveFedora::Base.find(member_id)
+        member = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: member_id, use_valkyrie: false)
         message = Hyrax::MultipleMembershipChecker.new(item: member).check(collection_ids: id, include_current_members: true)
         if message
           member.errors.add(:collections, message)
@@ -70,8 +74,16 @@ module Hyrax
       end
     end
 
+    # @return [Enumerable<ActiveFedora::Base>] an enumerable over the children of this collection
     def member_objects
       ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id}")
+    end
+
+    # Use this query to get the ids of the member objects (since the containment
+    # association has been flipped)
+    def member_object_ids
+      return [] unless id
+      member_objects.map(&:id)
     end
 
     def to_s
@@ -104,14 +116,15 @@ module Hyrax
       # This governs which partial to draw when you render this type of object
       def _to_partial_path #:nodoc:
         @_to_partial_path ||= begin
-                                element = ActiveSupport::Inflector.underscore(ActiveSupport::Inflector.demodulize(name))
-                                collection = ActiveSupport::Inflector.tableize(name)
-                                "hyrax/#{collection}/#{element}".freeze
-                              end
+          element = ActiveSupport::Inflector.underscore(ActiveSupport::Inflector.demodulize(name))
+          collection = ActiveSupport::Inflector.tableize(name)
+          "hyrax/#{collection}/#{element}"
+        end
       end
 
       def collection_type_gid_document_field_name
-        "collection_type_gid_ssim"
+        Deprecation.warn('use Hyrax.config.collection_type_index_field instead')
+        Hyrax.config.collection_type_index_field
       end
     end
 
@@ -181,19 +194,18 @@ module Hyrax
 
     def permission_template_read_users
       (permission_template.agent_ids_for(access: 'view', agent_type: 'user') +
-          permission_template.agent_ids_for(access: 'deposit', agent_type: 'user')).uniq
+        permission_template.agent_ids_for(access: 'deposit', agent_type: 'user')).uniq
     end
 
     def permission_template_read_groups
       (permission_template.agent_ids_for(access: 'view', agent_type: 'group') +
-          permission_template.agent_ids_for(access: 'deposit', agent_type: 'group')).uniq -
-          [::Ability.registered_group_name, ::Ability.public_group_name]
+        permission_template.agent_ids_for(access: 'deposit', agent_type: 'group')).uniq -
+        [::Ability.registered_group_name, ::Ability.public_group_name]
     end
 
     def visibility_group
       return [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_PUBLIC] if visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
       return [::Ability.registered_group_name] if visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-      # return [Hydra::AccessControls::AccessRight::PERMISSION_TEXT_VALUE_AUTHENTICATED] if visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
       []
     end
 
