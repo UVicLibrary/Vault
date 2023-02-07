1db4b6409e66b858ae84793066416cd11d3301a8
Tiffany Chan <tjychan@uvic.ca>
2023-02-07 11:02:46 -0800

    Add optional parameters to FixityCheckRecentWorksJob. This makes it possible to pass in different dates.

--- app/jobs/fixity_check_recent_works_job.rb
+++ app/jobs/fixity_check_recent_works_job.rb
@@ -4,27 +4,31 @@ class FixityCheckRecentWorksJob < ActiveJob::Base
 
   # @param [Date] start_date a DateTime object
   # @param [Date] end_date a DateTime object
-  def perform
+  def perform(start_date = six_months_ago, end_date = three_months_ago)
     # Run fixity checks
-    FixityCheckJob.perform_later(get_works)
+    FixityCheckJob.perform_later(get_works(start_date, end_date))
   end
 
   private
 
-    # @return a DateTime object
-    def start_date
-      (Date.today.beginning_of_month - 5.months).midnight.strftime("%FT%H:%M:%SZ")
+    # @return a DateTime object in Solr format
+    def six_months_ago
+      (Date.today.beginning_of_month - 6.months).midnight.strftime("%FT%H:%M:%SZ")
     end
 
-    # @return a DateTime object
-    def end_date
-      (Date.today.beginning_of_month - 2.months).midnight.strftime("%FT%H:%M:%SZ")
+    # @return a DateTime object in Solr format
+    def three_months_ago
+      (Date.today.beginning_of_month - 3.months).midnight.strftime("%FT%H:%M:%SZ")
     end
 
     # @return [Array < GenericWork >] array of works to pass to fixity check
-    def get_works
-      # Get all works uploaded between the start date and end date
-      solr = RSolr.connect url: Account.find_by(tenant: Apartment::Tenant.current).solr_endpoint.url
+    def get_works(start_date, end_date)
+      if Settings.multitenancy.enabled?
+       # Get all works uploaded between the start date and end date
+        solr = RSolr.connect url: Account.find_by(tenant: Apartment::Tenant.current).solr_endpoint.url
+      else
+        solr = RSolr.connect url: Settings.solr.url
+      end
       response = solr.get 'select', params: {
           q: "*:*",
           fq: ["has_model_ssim:GenericWork","system_create_dtsi:[#{start_date} TO #{end_date}}"],
