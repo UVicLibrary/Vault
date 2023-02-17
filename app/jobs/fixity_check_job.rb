27b434607dcae2159101352b42031aa6bd447dba
eltiffster <tjychan@uvic.ca>
2023-02-17 14:28:19 -0800

    Fix log file IO error and rescue/log NoMethodError.

--- app/jobs/fixity_check_job.rb
+++ app/jobs/fixity_check_job.rb
@@ -3,34 +3,36 @@ class FixityCheckJob < ActiveJob::Base
 	def perform(work_ids)
 		filename = DateTime.now.strftime('%Y%m%d%H%M%S')
 		log_file = File.new("/usr/local/rails/vault/log/fixity/#{filename}.log", 'w')
-		log_file.puts("Starting Fixity Checking")
+
 		# Create a list of failed file sets for emailer
 		failed = []
-		work_ids.each do |id|
-			GenericWork.find(id).file_sets.each do |fs|
-				puts fs.id
-				# If a file set passed a fixity check within the last month, no need to check again.
-				next if passed_recent_check?(fs)
-				begin
-					fixity = ActiveFedora::FixityService.new fs.latest_content_version.uri
-				rescue NoMethodError => error
-					log_file.puts "#{fs.id} has no files attached"
-					failed.push(fs)
-					next
+		File.open(log_file, "w+") do |file|
+			file.puts("Starting Fixity Checking")
+			work_ids.each do |id|
+				GenericWork.find(id).file_sets.each do |fs|
+					puts fs.id
+					# If a file set passed a fixity check within the last month, no need to check again.
+					next if passed_recent_check?(fs)
+					begin
+						fixity = ActiveFedora::FixityService.new fs.latest_content_version.uri
+					rescue NoMethodError => error
+						file.puts "#{fs.id} has no files attached"
+						failed.push(fs)
+						next
+					end
+					unless fixity.check
+						file.puts("#{fs.id} has a possible corruption")
+						failed.push(fs)
+					end
+					fs.last_fixity_check = filename
+					fs.save
 				end
-				unless fixity.check
-					log_file.puts("#{fs.id} has a possible corruption")
-					failed.push(fs)
-				end
-				fs.last_fixity_check = filename
-				fs.save
+				file.puts("Finished Fixity Checking")
 			end
-			log_file.puts("Finished Fixity Checking")
-			log_file.close
+		end
+		if failed.any?
 			# Mail email addresses defined in config/settings.yml
-			if failed.any?
-				::NotificationMailer.with(file_sets: failed).fixity_failures.deliver
-			end
+			::NotificationMailer.with(file_sets: failed).fixity_failures.deliver
 		end
 	end
 
@@ -43,7 +45,7 @@ class FixityCheckJob < ActiveJob::Base
 	def passed?(file_set)
 		log_path = "/usr/local/rails/vault/log/fixity/#{file_set.last_fixity_check}.log"
 		return false unless File.file? log_path
-		File.read(log_path).exclude?(file_set.id)
+		File.read(log_path).exclude?(file_set.id) && File.read(log_path).include?("Finished Fixity Checking")
 	end
 
 end
