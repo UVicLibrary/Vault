f0c39d10c2bc235a346d35f7d86d4d0711414b9f
Ethan Getty <35879251+EGetty@users.noreply.github.com>
2023-02-28 15:44:06 -0800

    Fix error where featuring collections would throw an error on first load

--- app/controllers/hyrax/featured_collections_controller.rb
+++ app/controllers/hyrax/featured_collections_controller.rb
@@ -26,5 +26,32 @@ module Hyrax
       end
     end
 
+    # Similar to the "create" command, "index" handles the response as a redirect_to statement as opposed
+    # to a render statement
+    def index
+      authorize! :create, FeaturedCollection
+      @featured_collection = FeaturedCollection.new(collection_id: params[:collection_id])
+
+      respond_to do |format|
+        if @featured_collection.save
+          format.json {
+            redirect_to dashboard_collection_path(id: params[:collection_id], format: 'html')
+          }
+        else
+          format.json { render json: @featured_collection.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # Similar to the "destroy" command, "show" handles the response as a redirect_to as opposed
+    # to a json response
+    def show
+      authorize! :destroy, FeaturedCollection
+      @featured_collection = FeaturedCollection.find_by(collection_id: params[:collection_id])
+      @featured_collection&.destroy
+
+      redirect_to dashboard_collection_path(id: params[:collection_id], format: 'html')
+    end
+
   end
 end
