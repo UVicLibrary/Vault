58e0ee36b99b8e327313ca774e12943b63363b93
Tiffany Chan <tjychan@uvic.ca>
2023-03-21 16:18:12 -0700

    Fast Update: add model validation (regex) for valid URI.

--- app/models/fast_update/change.rb
+++ app/models/fast_update/change.rb
@@ -1,8 +1,8 @@
 class FastUpdate::Change < ApplicationRecord
   attribute :count, :integer, default: 0
 
-  # validates :old_label, presence: true
-  validates :old_uri, presence: true
+  # Exclude :old_label validation because user can paste in a deprecated URI without selecting a label.
+  validates :old_uri, presence: true, format: { with: /\Ahttp:\/\/id.worldcat.org\/fast\/\d+\z/, message: "is not a valid URI." }
   # [Optional:] exclusion: { in: ->(repl) { repl.new_uris }, message: "New uri cannot be the same as old one" }
   validates :action, presence: true
   validates :new_uris, presence: { if: Proc.new { |r| r.action == "replace" }, message: "must be selected to replace the old one" }
