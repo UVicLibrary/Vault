c42c75c5f89e78a7606aa34543c026bb9a7ec427
Tiffany Chan <tjychan@uvic.ca>
2023-03-28 17:06:38 -0700

    Fast Update: add inclusion validation to FastUpdate::Change. Add model and helper specs. Make some helper methods private.

--- app/models/fast_update/change.rb
+++ app/models/fast_update/change.rb
@@ -4,7 +4,7 @@ class FastUpdate::Change < ApplicationRecord
   # Exclude :old_label validation because user can paste in a deprecated URI without selecting a label.
   validates :old_uri, presence: true, format: { with: /\Ahttp:\/\/id.worldcat.org\/fast\/\d+\z/, message: "is not a valid URI." }
   # [Optional:] exclusion: { in: ->(repl) { repl.new_uris }, message: "New uri cannot be the same as old one" }
-  validates :action, presence: true
+  validates :action, presence: true, inclusion: { in: ["replace", "delete"] }
   validates :new_uris, presence: { if: Proc.new { |r| r.action == "replace" }, message: "must be selected to replace the old one" }
   validates :collection_id, presence: true
 end
--- lib/fast_update/fast_update_helper.rb
+++ lib/fast_update/fast_update_helper.rb
@@ -19,29 +19,6 @@ module FastUpdateHelper
     end
   end
 
-  # == Render "Fields Containing URI" Table Cell in Search Results
-  # @!group
-
-  def work_types
-    Hyrax::QuickClassificationQuery.new(current_user).authorized_models
-  end
-
-  # @return [Hash] an array of solrized controlled property fields organized by work type
-  # Example: { "GenericWork" => ["based_near_tesim",...] }. You can override this with your own hash
-  # of models and field names
-  def solr_field_names
-    work_types.each_with_object({}) do |model, hash|
-      hash[model.to_s] = model.controlled_properties.map { |prop| "#{prop}_tesim" }
-    end
-  end
-
-  # Convert the solr field name into human-readable format for display
-  # @param [String] - the solr field name, e.g. "based_near_tesim"
-  # @return [String] - human-readable field name, e.g. "Based near"
-  def desolrize(field)
-    field.gsub('_tesim','').gsub('_',' ').capitalize
-  end
-
   # Render the contents of "Fields containing URI" table cell in views/fast_update/changes/_list_works
   # @param [SolrDocument]
   # @param [String] - the uri to search for
@@ -50,12 +27,6 @@ module FastUpdateHelper
     field_names = solr_field_names[model].select { |field| document.has_key?(field) && document[field].include?(uri) }
     field_names.map { |field| desolrize(field) }.join(', ')
   end
-
-  def model_field_name
-    "has_model_ssim"
-  end
-
-  # @!endgroup
 
   #== Override Blacklight Facets
   # See https://workshop.projectblacklight.org/v7.11.1/helper-method-overrides/
@@ -76,26 +47,11 @@ module FastUpdateHelper
     end + render_facet_count(item.hits)
   end
 
-  ##
-  # Where should this facet link to?
-  # @param [Blacklight::Solr::Response::Facets::FacetField] facet_field
-  # @param [String] item
-  # @return [String]
-  def path_for_facet(facet_field, item)
-    facet_config = facet_configuration_for_field(facet_field)
-    if facet_config.url_method
-      send(facet_config.url_method, facet_field, item)
-    else
-      fast_update_search_preview_path(search_state.add_facet_params_and_redirect(facet_field, item))
-    end
-  end
-
   # @param [String] - facet field name
   # @param [Blacklight::Solr::Response::Facets::FacetItem]
   def search_preview_path(facet_field, item)
     fast_update_search_preview_path(search_state.add_facet_params_and_redirect(facet_field, item))
   end
-  # @!endgroup
 
   #== Override Blacklight Constraints
   # See https://workshop.projectblacklight.org/v7.11.1/helper-method-overrides/
@@ -132,7 +88,47 @@ module FastUpdateHelper
   # @option options [Array<String>] :classes an array of classes to add to container span for constraint.
   # @return [String]
   def render_constraint_element(label, value, options = {})
-    render(:partial => "constraints_element", :locals => {:label => label, :value => value, :options => options})
+    render(:partial => "fast_update/changes/constraints_element", :locals => {:label => label, :value => value, :options => options})
   end
+
+  private
+
+    def model_field_name
+      "has_model_ssim"
+    end
+
+    def work_types
+      Hyrax::QuickClassificationQuery.new(current_user).authorized_models
+    end
+
+  # @return [Hash] an array of solrized controlled property fields organized by work type
+  # Example: { "GenericWork" => ["based_near_tesim",...] }. You can override this with your own hash
+  # of models and field names
+    def solr_field_names
+      work_types.each_with_object({}) do |model, hash|
+        hash[model.to_s] = model.controlled_properties.map { |prop| "#{prop}_tesim" }
+      end
+    end
+
+  # Convert the solr field name into human-readable format for display
+  # @param [String] - the solr field name, e.g. "based_near_tesim"
+  # @return [String] - human-readable field name, e.g. "Based near"
+    def desolrize(field)
+      field.gsub('_tesim','').gsub('_',' ').capitalize
+    end
+
+  ##
+  # Where should this facet link to?
+  # @param [Blacklight::Solr::Response::Facets::FacetField] facet_field
+  # @param [String] item
+  # @return [String]
+    def path_for_facet(facet_field, item)
+      facet_config = facet_configuration_for_field(facet_field)
+      if facet_config.url_method
+        send(facet_config.url_method, facet_field, item)
+      else
+        fast_update_search_preview_path(search_state.add_facet_params_and_redirect(facet_field, item))
+      end
+    end
 
 end
