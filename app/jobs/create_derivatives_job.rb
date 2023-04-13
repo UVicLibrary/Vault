39dd41f74d0c6cba10c303e8ca180c59b94a6eb0
Tiffany Chan <tjychan@uvic.ca>
2023-04-13 09:54:11 -0700

    Add export_new_files method to CreateDerivativesJob.

--- app/jobs/create_derivatives_job.rb
+++ app/jobs/create_derivatives_job.rb
@@ -7,15 +7,12 @@ class CreateDerivativesJob < Hyrax::ApplicationJob
   def perform(file_set, file_id, filepath = nil)
     return if file_set.video? && !Hyrax.config.enable_ffmpeg
     filename = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id, filepath)
-
-    Sidekiq.logger.info "filename: #{filename}"
     file_set.create_derivatives(filename)
-    # Run the service again?
-
     # Reload from Fedora and reindex for thumbnail and extracted text
     file_set.reload
     file_set.update_index
     file_set.parent.update_index if parent_needs_reindex?(file_set)
+    export_new_files(file_set)
   end
 
   # If this file_set is the thumbnail for the parent work,
@@ -23,5 +20,12 @@ class CreateDerivativesJob < Hyrax::ApplicationJob
   def parent_needs_reindex?(file_set)
     return false unless file_set.parent
     file_set.parent.thumbnail_id == file_set.id
+  end
+
+  # If the parent work was created more than 3 months before the file set, export the newly-attached file
+  def export_new_files(file_set)
+    if file_set.parent.create_date < 3.months.ago
+      ExportFileJob.perform_later(file_set)
+    end
   end
 end
