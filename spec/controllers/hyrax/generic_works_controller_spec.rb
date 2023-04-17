fe3c7d9e0bd0bf3f1b8e0eea2cafb3c12e646ccc
Tiffany Chan <tjychan@uvic.ca>
2023-04-17 16:34:30 -0700

    Update generic_works_controller_spec.rb.
    This now more closely matches with later versions of Hyku/Hyrax.

--- spec/controllers/hyrax/generic_works_controller_spec.rb
+++ spec/controllers/hyrax/generic_works_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Hyrax::GenericWorksController do
   let(:user) { FactoryBot.create(:user) }
   let(:work) { FactoryBot.create(:work_with_one_file, user: user) }
@@ -9,33 +11,17 @@ RSpec.describe Hyrax::GenericWorksController do
                                         :original_file)
   end
 
-  describe '#manifest' do
-    let(:manifest_factory) { double(to_h: { test: 'manifest' }) }
-
-    before do
-      sign_in user
-      allow(IIIFManifest::ManifestFactory).to receive(:new)
-        .with(Hyku::ManifestEnabledWorkShowPresenter)
-        .and_return(manifest_factory)
-    end
-
-    it "produces a manifest" do
-      get :manifest, params: { id: work, format: :json }
-      expect(response.body).to eq "{\"test\":\"manifest\"}"
-    end
-  end
-
   describe "#presenter" do
+    subject { controller.send :presenter }
+
     let(:solr_document) { SolrDocument.new(FactoryBot.create(:generic_work).to_solr) }
 
     before do
       allow(controller).to receive(:curation_concern_from_search_results).and_return(solr_document)
     end
 
-    subject { controller.send :presenter }
-
     it "initializes a presenter" do
-      expect(subject).to be_kind_of Hyku::ManifestEnabledWorkShowPresenter
+      expect(subject).to be_kind_of VaultWorkShowPresenter
       expect(subject.manifest_url).to eq "http://test.host/concern/generic_works/#{solr_document.id}/manifest"
     end
   end
