ba0c2c81f4bbe9d8b52664a8e7e3e26d5a152872
Tiffany Chan <tjychan@uvic.ca>
2023-05-04 14:50:11 -0700

    Delete already commented-out code from splash_controller.

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,8 +1,6 @@
+# frozen_string_literal: true
+
 class SplashController < ProprietorController
-  
-  #def create_work_presenter
-  #   @create_work_presenter ||= Hyrax::SelectTypeListPresenter.new(current_user)
-  #end
-  #helper_method :create_work_presenter
+
   def index; end
 end
