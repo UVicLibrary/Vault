b73a23135dc4c87d02a9b98632dc84ace5a183af
Tiffany Chan <tjychan@uvic.ca>
2023-05-04 13:52:50 -0700

    Update Characterize job to match the file in Hyrax 3.

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class CharacterizeJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.ingest_queue_name
 
@@ -7,14 +8,28 @@ class CharacterizeJob < Hyrax::ApplicationJob
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
   def perform(file_set, file_id, filepath = nil)
-    # Sidekiq::JobLogger.info "Hit CharacterizeJob for #{file_set.id}"
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
     filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
-    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
-    # Sidekiq::JobLogger.info "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
-    file_set.characterization_proxy.save!
-    file_set.update_index
-    # Sidekiq::JobLogger.info "Updated and indexed #{file_set.id}"
+    characterize(file_set, file_id, filepath)
     CreateDerivativesJob.perform_later(file_set, file_id, filepath)
   end
+
+  private
+
+  def characterize(file_set, _file_id, filepath)
+    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
+    Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
+    file_set.characterization_proxy.alpha_channels = channels(filepath) if file_set.image? && Hyrax.config.iiif_image_server?
+    file_set.characterization_proxy.save!
+    file_set.update_index
+  end
+
+  def channels(filepath)
+    ch = MiniMagick::Tool::Identify.new do |cmd|
+      cmd.format '%[channels]'
+      cmd << filepath
+    end
+    [ch]
+  end
+
 end
