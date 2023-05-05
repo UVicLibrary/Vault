fd7af9db5698011746fe64f0eb48b410bf953379
Tiffany Chan <tjychan@uvic.ca>
2023-05-04 17:44:37 -0700

    Delete unnecessary files in migrate-old.

--- db/migrate-old/20190119004202_create_featured_collection_lists.rb
+++
@@ -1,8 +0,0 @@
-class CreateFeaturedCollectionLists < ActiveRecord::Migration[5.1]
-  def change
-    create_table :featured_collection_lists do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate-old/20190130224916_add_details_to_content_blocks.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailsToContentBlocks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :content_blocks, :researcher_name, :string
-    add_column :content_blocks, :researcher_title, :string
-  end
-end
