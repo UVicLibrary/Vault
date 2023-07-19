b29218b8ce1694b700b19b6b594d85f92c5ddce3
Tiffany Chan <tjychan@uvic.ca>
2023-07-19 09:58:41 -0700

    Update collection thumbnail path service.
    This service previously checked for an image derivative that we're no longer using/generating. I've removed that check since we've switched to IIIF thumbnail paths for images.

--- app/services/concerns/large_iiif_thumbnail_paths.rb
+++ app/services/concerns/large_iiif_thumbnail_paths.rb
@@ -8,9 +8,8 @@ module LargeIIIFThumbnailPaths
     # @return the IIIF url for the thumbnail if it's an image, otherwise gives
     #         the thumbnail download path
     def thumbnail_path(file_set, size = '!500,900'.freeze)
-      # return super(file_set) unless file_set.image?
+	#iiif_thumbnail_path is defined in app/services/concerns/iiif_thumbnail_paths.rb
       iiif_thumbnail_path(file_set, '!500,900')
     end
   end
-
 end
