26b0205362c1bef9db05d126f7fed3a2ed0393a0
Tiffany Chan <tjychan@uvic.ca>
2023-07-19 09:58:41 -0700

    Update collection_thumbnail_path_service.rb

--- app/services/collection_thumbnail_path_service.rb
+++ app/services/collection_thumbnail_path_service.rb
@@ -1,21 +1,18 @@
-class CollectionThumbnailPathService < Hyrax::CollectionThumbnailPathService
+class CollectionThumbnailPathService < Hyrax::ThumbnailPathService
   # Returns the path for collection thumbnails, i.e.
   #   1. The path for an uploaded collection thumbnail, if any
   #   2. A larger resolution path for an IIIF thumbnail than the default option
-  #       (originally this is 100x300). This is so that it still looks in a
-  #       homepage card.
+  #  (originally this is 100x300). This is so that it still looks good
+  #   on a homepage card.
 
-  include IIIFThumbnailPaths
 
   class << self
     # @param [Collection] object to get the thumbnail path for an uploaded image
     def call(object)
       if uploaded_thumbnail?(object)
         "/uploaded_collection_thumbnails/#{object.id}/#{object.id}_card.jpg"
-      elsif object.thumbnail.present?
-        thumbnail_path(object.thumbnail, '!500,900')
       else
-        default_image
+        IIIFCollectionThumbnailPathService.call(object)
       end
     end
 
@@ -26,15 +23,6 @@ class CollectionThumbnailPathService < Hyrax::CollectionThumbnailPathService
     def upload_dir(collection)
       "#{Rails.root.to_s}/public/uploaded_collection_thumbnails/#{collection.id}"
     end
-
-    # @param [FileSet] file_set
-    # @param [String] size ('!500,900') an IIIF image size defaults to an image no
-    #                      wider than 150px and no taller than 300px
-    # @return the IIIF url for the thumbnail if it's an image, otherwise gives
-    #         the thumbnail download path
-    # def thumbnail_path(file_set, size = '!500,900'.freeze)
-    #   iiif_thumbnail_path(file_set, size)
-    # end
 
   end
 end
