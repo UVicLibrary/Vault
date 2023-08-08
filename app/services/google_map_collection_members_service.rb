c10fdb63b172acc673d47cd827f433f0b69c6dc6
Tiffany Chan <tjychan@uvic.ca>
2023-08-08 16:47:22 -0700

    Update GoogleMapCollectionMembersService and delete unused partial.

--- app/services/google_map_collection_members_service.rb
+++ app/services/google_map_collection_members_service.rb
@@ -1,10 +1,15 @@
 class GoogleMapCollectionMembersService < Hyrax::Collections::CollectionMemberService
 
+  # @return [Blacklight::SolrResponse]
+  # Member works of a collection that have something in the coordinates field.
+  # Call response.documents to view the Solr documents.
+  # Call builder.query to see the params sent to Solr
   def available_member_works
-    query_solr_with_field_selection(
-        query_builder: works_search_builder.with(fq: 'coordinates_tesim: [* TO *]', rows: 3000),
-        fl: filter_fields
-    )
+    query_solr(query_builder: works_search_builder.merge(
+                                  fq: "coordinates_tesim:[* TO *]",
+                                  fl: filter_fields,
+                                  rows: 3000),
+               query_params: {})
   end
 
   def filter_fields
