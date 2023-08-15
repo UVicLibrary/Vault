25cd12cb36f8369a57d75665e29f3321f781d698
eltiffster <tjychan@uvic.ca>
2023-08-15 16:27:24 -0700

    Fix bug with BatchAssignWorkDOIsJob.

--- app/jobs/batch_assign_work_dois_job.rb
+++ app/jobs/batch_assign_work_dois_job.rb
@@ -3,7 +3,7 @@ class BatchAssignWorkDOIsJob < ApplicationJob
   # @param [Array <GenericWork>]
   # To get all works in a collection, use Collection.find(id).member_works
   def perform(works)
-    works do |work|
+    works.each do |work|
       next if work.doi.present?
       AssignWorkDOIJob.perform_later(work)
     end
