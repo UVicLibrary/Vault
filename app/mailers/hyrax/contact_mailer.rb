53f445e7ad1659bc5dc9f125eb7206ebb016c1e0
eltiffster <tjychan@uvic.ca>
2023-09-18 11:05:26 -0700

    Add comment to Hyrax contact_mailer.

--- app/mailers/hyrax/contact_mailer.rb
+++ app/mailers/hyrax/contact_mailer.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module Hyrax
-  # Mailer for contacting the administrator
+  # Mailer for contacting the administrator. The "to" email address is set
+  # via account.contact_email. To change the address, call
+  # account.contact_mail = adminstrator@example.com
   class ContactMailer < HykuMailer
     def contact(contact_form)
       @contact_form = contact_form
