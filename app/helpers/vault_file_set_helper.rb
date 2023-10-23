a5486bca33f51418da9399e814f5db622ac5985d
eltiffster <tjychan@uvic.ca>
2023-10-23 15:32:54 -0700

    Remove deprecated method from vault_file_set_helper.rb

--- app/helpers/vault_file_set_helper.rb
+++ app/helpers/vault_file_set_helper.rb
@@ -8,23 +8,8 @@ module VaultFileSetHelper
       can?(:edit, file_set.id) or file_set.parent.downloadable?
     else
       Hyrax.config.display_media_download_link? &&
-          can?(:download, file_set)
+          can?(:read, file_set)
     end
-  end
-
-  ##
-  # @deprecated use render(media_display_partial(file_set), file_set: file_set)
-  #   instead
-  #
-  # @param presenter [Object]
-  # @param locals [Hash{Symbol => Object}]
-  def media_display(presenter, locals = {})
-    Deprecation.warn("the helper `media_display` renders a partial name " \
-                     "provided by `media_display_partial`. Callers " \
-                     "should render `media_display_partial(file_set) directly
-                     instead.")
-
-    render(media_display_partial(presenter), locals.merge(file_set: presenter))
   end
 
   # Override Hyrax::FileSetHelper in Hyrax v. 3.1
