142e69162b338e8fb84a2435f7998d9bd5013375
Ethan Getty <35879251+EGetty@users.noreply.github.com>
2023-12-18 13:55:20 -0800

    Update views to use collections_decorator methods
    Includes quick update of collections_decorator.rb to properly display order of parent/child collections in drop down

--- app/decorators/helpers/hyrax/collections_decorator.rb
+++ app/decorators/helpers/hyrax/collections_decorator.rb
@@ -4,8 +4,12 @@ Hyrax::CollectionsHelper.module_eval do
   # @since 3.1.0
   # @return [Array<SolrDocument>]
   def available_child_collections(collection:)
-    Hyrax::Collections::NestedCollectionQueryService
+    return @available_children if @available_children.present?
+    colls = Hyrax::Collections::NestedCollectionQueryService
       .available_child_collections(parent: collection, scope: controller, limit_to_id: nil).sort_by{ |coll| coll["title_sort_ssi"]}
+    @available_children = colls.map do |col|
+      { "id" => col.id, "title_first" => col.title.first }
+    end.to_json
   end
 
   ##
@@ -16,7 +20,11 @@ Hyrax::CollectionsHelper.module_eval do
   # @return [String] JSON document containing id/title pairs for eligible
   #   parent collections to be displayed in an "Add to Collection" dropdown
   def available_parent_collections_data(collection:)
-    Hyrax::Collections::NestedCollectionQueryService
-      .available_parent_collections(child: collection, scope: controller, limit_to_id: nil)
+    return @available_parents if @available_parents.present?
+    colls = Hyrax::Collections::NestedCollectionQueryService
+      .available_parent_collections(child: collection, scope: controller, limit_to_id: nil).sort_by{ |coll| coll["title_sort_ssi"]}
+    @available_parents = colls.map do |col|
+      { "id" => col.id, "title_first" => col.title.first }
+    end.to_json
   end
 end
