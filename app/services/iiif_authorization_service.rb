5988026cf94cb8b80b395f9a3952c6608a2a4c8d
Tiffany Chan <tjychan@uvic.ca>
2024-01-08 11:00:40 -0800

    Update IIIF authorization service to avoid deprecated method.

--- app/services/iiif_authorization_service.rb
+++ app/services/iiif_authorization_service.rb
@@ -8,32 +8,14 @@ class IIIFAuthorizationService < Hyrax::IIIFAuthorizationService
   # RIIIF gem docs on authorization:
   # https://github.com/sul-dlss/riiif#authorization
 
-  attr_reader :controller
-  def initialize(controller)
-    @controller = controller
-  end
-
-  def can?(_action, object)
-    if image_path? && !thumbnail?
-      @controller.current_ability.can?(:download, file_set_id_for(object))
-    else
-      @controller.current_ability.can?(:show, file_set_id_for(object))
-    end
-  end
-
   private
 
   def file_set_id_for(object)
-    URI.decode(object.id).split('/').first
+    URI.decode_www_form_component(object.id).to_s.split('/').first
   end
 
-  def image_path?
-    @controller.instance_of?(Riiif::ImagesController) &&
-        @controller.action_name == "show"
-  end
-
-  def thumbnail?
-    @controller.params[:size] == IIIFThumbnailPaths::THUMBNAIL_SIZE ||
-        @controller.params[:size] == LargeIIIFThumbnailPaths::LARGE_THUMBNAIL_SIZE
-  end
+  # def thumbnail?
+  #   @controller.params[:size] == IIIFThumbnailPaths::THUMBNAIL_SIZE ||
+  #       @controller.params[:size] == LargeIIIFThumbnailPaths::LARGE_THUMBNAIL_SIZE
+  # end
 end
