ed337e7499c4a5d5c247bf55993029d24e4314a1
Tiffany Chan <tjychan@uvic.ca>
2024-01-23 17:01:45 -0800

    Update IIIFAuthorizationService.

--- spec/services/iiif_authorization_service_spec.rb
+++ spec/services/iiif_authorization_service_spec.rb
@@ -7,101 +7,91 @@ RSpec.describe IIIFAuthorizationService do
   let(:file_set_id) { 'mp48sc763' }
   let(:image_id) { "#{file_set_id}/files/0b957460-99b4-4c31-902f-0fc23eefb972" }
   let(:image) { Riiif::Image.new(image_id) }
+  let(:referer) { nil }
+  let(:size) { 'full,full' }
 
-  before { allow(controller).to receive(:current_ability).and_return(ability) }
+  before do
+    allow(controller).to receive(:current_ability).and_return(ability)
+    allow(controller.request).to receive(:referer).and_return(referer)
+    allow(controller).to receive(:params).and_return(size: size)
+  end
 
   describe "#can?" do
-    context "when the user has read access to the FileSet" do
-      before { allow(ability).to receive(:test_read).with(file_set_id).and_return(true) }
-
-      context "info" do
-        subject { service.can?(:info, image) }
-
-        it { is_expected.to be true }
-      end
-
-      context "show" do
-        subject { service.can?(:show, image) }
-
-        it { is_expected.to be true }
-      end
-    end
 
     context "when the user doesn't have read access to the FileSet" do
-      before { allow(ability).to receive(:test_read).with(file_set_id).and_return(false) }
 
-      context "info" do
-        subject { service.can?(:info, image) }
-
-        it { is_expected.to be false }
+      before do
+        allow(ability).to receive(:test_read).with(file_set_id).and_return(false)
+        allow(ability).to receive(:test_download).with(file_set_id).and_return(false)
       end
+
+      context 'when requesting an image' do
+        context "info" do
+          subject { service.can?(:info, image) }
+
+          it { is_expected.to be false }
+        end
+
+        context "show" do
+          subject { service.can?(:show, image) }
+
+          it { is_expected.to be false }
+        end
+      end
+
+      context 'when requesting a thumbnail' do
+        context 'work thumbnail' do
+          let(:size) { IIIFThumbnailPaths::THUMBNAIL_SIZE }
+          context "show" do
+            subject { service.can?(:show, image) }
+            it { is_expected.to be true }
+          end
+        end
+      end
+    end
+
+    context 'when the user has read access but not download access to the FileSet' do
+      subject { service.can?(:show, image) }
+
+      before do
+        allow(ability).to receive(:test_read).with(file_set_id).and_return(true)
+        allow(ability).to receive(:test_download).with(file_set_id).and_return(false)
+      end
+
+      context 'when requesting a thumbnail' do
+
+        context 'work thumbnail' do
+          let(:size) { IIIFThumbnailPaths::THUMBNAIL_SIZE }
+          context "show" do
+            it { is_expected.to be true }
+          end
+        end
+      end
+
+      context 'when requesting a full image' do
+        context "show" do
+          it { is_expected.to be false }
+        end
+      end
+
+      context 'when on a universal viewer page' do
+        let(:referer) { "http://example.com/uv/uv-no-download.html" }
+        context "show" do
+          it { is_expected.to be true }
+        end
+      end
+
+    end
+
+    context 'when the user has download access to the FileSet' do
+      let(:user) { create(:admin) }
 
       context "show" do
         subject { service.can?(:show, image) }
 
-        it { is_expected.to be false }
+        it { is_expected.to be true }
       end
     end
-
-    # context 'when the user has read access but not download access to the FileSet' do
-    #   subject { service.can?(:show, image) }
-    #
-    #   before do
-    #     allow(ability).to receive(:test_read).with(file_set_id).and_return(true)
-    #     allow(ability).to receive(:test_download).with(file_set_id).and_return(false)
-    #     allow(controller).to receive(:action_name).and_return('show')
-    #   end
-    #
-    #   context 'when requesting a thumbnail' do
-    #     before { allow(controller).to receive(:params).and_return(size: size) }
-    #
-    #     context 'work thumbnail' do
-    #       let(:size) { IIIFThumbnailPaths::THUMBNAIL_SIZE }
-    #
-    #       context "show" do
-    #         it { is_expected.to be true }
-    #       end
-    #     end
-    #
-    #     context 'collection or card thumbnail' do
-    #       let(:size) { LargeIIIFThumbnailPaths::LARGE_THUMBNAIL_SIZE }
-    #
-    #       context "show" do
-    #         it { is_expected.to be true }
-    #       end
-    #     end
-    #
-    #   end
-    #
-    #   context 'when on a work page' do
-    #     let(:controller) { Hyrax::GenericWorksController.new }
-    #
-    #     context "show" do
-    #       it { is_expected.to be true }
-    #     end
-    #
-    #   end
-    #
-    #   # context 'when requesting a full image' do
-    #   #   before { allow(controller).to receive(:params).and_return(size: size) }
-    #   #   let(:size) { 'full,full' }
-    #   #
-    #   #   context "show" do
-    #   #     it { is_expected.to be false }
-    #   #   end
-    #   # end
-    #
-    # end
-
-    # context 'when the user has download access to the FileSet' do
-    #   let(:user) { create(:admin) }
-    #
-    #   context "show" do
-    #     subject { service.can?(:show, image) }
-    #
-    #     it { is_expected.to be true }
-    #   end
-    # end
   end
 
   describe "#file_set_id_for" do
