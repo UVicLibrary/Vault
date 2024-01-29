141917a85528141dd1bcf2d96a3da3604c0c81af
Tiffany Chan <tjychan@uvic.ca>
2024-01-29 15:04:57 -0800

    Update ToggleDownloadsJob to change works' permissions attributes.

--- app/jobs/toggle_downloads_job.rb
+++ app/jobs/toggle_downloads_job.rb
@@ -1,17 +1,46 @@
 class ToggleDownloadsJob < Hyrax::ApplicationJob
-
-  # Allow or disallow downloads for an entire collection.
-  # downloadable = true/false
+  # Enable/disable public downloads for all of a collection's
+  # members (works and file sets).
+  #
+  # @param[String] - the collection ID
+  # @param[String] - email of the user who initiated the job (from dashboard/collections/edit)
+  # @param[Boolean] - whether to enable or disable downloads
   def perform(collection_id, user_email, downloadable)
-    collection = Collection.find(collection_id)
+    setting = ActiveModel::Type::Boolean.new.cast(downloadable)
     works = GenericWork.where(member_of_collection_ids_ssim: collection_id)
+
     works.each do |work|
-      next if work.downloadable == ActiveModel::Type::Boolean.new.cast(downloadable)
-      work.downloadable = ActiveModel::Type::Boolean.new.cast(downloadable)
+      permissions_hash = work.permissions.map(&:to_hash)
+
+      if setting == true
+        # For idempotency: no need to resave the object if permissions
+        # are already what they're supposed to be
+        next if permissions_hash.include?(public_download_access) && work.downloadable == setting
+        permissions_hash.push(public_download_access)
+      elsif setting == false
+        next if permissions_hash.exclude?(public_download_access) && work.downloadable == setting
+        permissions_hash.delete(public_download_access)
+      end
+
+      # Clear the work permissions hash first to prevent Hydra from
+      # creating duplicate relationships
+      work.permissions = []
+      work.permissions_attributes = permissions_hash.uniq
+      # For now we set both #downloadable and the permissions attributes.
+      # Later, we'll remove #downloadable entirely.
+      work.downloadable = setting
       work.save!
+
+      # File sets also need to reindex the download_access_groups_ssim key
+      # See app/services/indexes_download_permissions.rb
       work.file_sets.each(&:update_index)
     end
     DownloadsMailer.with(user_email: user_email.gsub('-dot-', '.'), id: collection_id, downloadable: downloadable).send_email.deliver_now
   end
 
+  private
+
+  def public_download_access
+    { name: "public", type: "group", access: "download" }
+  end
 end
