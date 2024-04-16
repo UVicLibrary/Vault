eed59ebf7e755312e438e51be651ca701ffc336e
eltiffster <tjychan@uvic.ca>
2024-04-16 12:27:05 -0700

    Update ReindexFileSetsJob.

--- app/jobs/reindex_file_sets_job.rb
+++ app/jobs/reindex_file_sets_job.rb
@@ -1,9 +1,13 @@
 class ReindexFileSetsJob < ActiveJob::Base
   # reindex all file sets in a collection
-  def perform(collection_id)
+  def perform(work_ids)
     works = GenericWork.where(member_of_collection_ids_ssim: collection_id)
-    works.each do |w|
-      w.file_sets.each { |fs| fs.update_index }
+    work_ids.each do |id|
+      work = GenericWork.find(id)
+      work.file_sets.each do |file_set|
+        next if SolrDocument.find(file_set.id).has_key?("download_access_group_ssim")
+        file_set.update_index
+      end
     end
   end
 
