b723baaa5d0c70022268170943bd3764cdc8b26c
eltiffster <tjychan@uvic.ca>
2024-04-23 10:35:11 -0700

    Ignore common error messages from bots that blow up our logs.

---
+++ config/initializers/logger.rb
@@ -0,0 +1,23 @@
+# spammers were blowing up our logs
+# this suppresses routing errors
+if Rails.env.production?
+  class ActionDispatch::DebugExceptions
+    alias_method :old_log_error, :log_error
+    def log_error(env, wrapper)
+      if ignored_error_classes.include? wrapper.exception.class
+        return
+      else
+        old_log_error env, wrapper
+      end
+    end
+
+    # To ignore an error class in the logs, add it to this array and
+    # restart Apache (sudo systemctl restart httpd)
+    def ignored_error_classes
+      [ActiveFedora::ObjectNotFoundError, ActionController::RoutingError,
+       Blacklight::Exceptions::RecordNotFound, Ldp::Gone, FrozenError,
+       Riiif::ConversionError, Hyrax::ObjectNotFoundError,
+       I18n::InvalidLocale]
+    end
+  end
+end
