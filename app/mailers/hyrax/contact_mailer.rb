e98fd57a24506614584ffcb1cebd287ccd0ef05f
Tiffany Chan <tjychan@uvic.ca>
2024-06-04 10:33:54 -0700

    Use reply_to header instead of from header for contact mailer.

--- app/mailers/hyrax/contact_mailer.rb
+++ app/mailers/hyrax/contact_mailer.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
+# OVERRIDE Hyrax 3.4.2 and Hyku v.2
+# Use :reply_to header instead of :from to avoid
+# errors hitting mail server
+
 module Hyrax
   # Mailer for contacting the administrator. The "to" email address is set
   # via account.contact_email. To change the address, call
   # account.contact_mail = adminstrator@example.com
   class ContactMailer < HykuMailer
+
     def contact(contact_form)
       @contact_form = contact_form
       # Check for spam
       return if @contact_form.spam?
       headers = @contact_form.headers.dup
       headers[:subject] += " [#{host_for_tenant}]"
+      headers[:reply_to] = headers.delete(:from)
+      Rails.logger.debug("headers = #{headers.inspect}")
       mail(headers)
     end
+
   end
 end
