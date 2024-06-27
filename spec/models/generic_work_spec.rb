b4db0b49987f5c3ef8e7ccedec94cd0f53a51caf
Tiffany Chan <tjychan@uvic.ca>
2024-06-27 14:47:55 -0700

    When destroying a work with a findable DOI, change the DOI status to registered before destroying the work.

--- spec/models/generic_work_spec.rb
+++ spec/models/generic_work_spec.rb
@@ -29,4 +29,44 @@ RSpec.describe GenericWork do
                                      :identifier, :based_near, :related_url,
                                      :bibliographic_citation, :source]) }
   end
+
+  let(:work) { create(:public_generic_work, doi_status_when_public: "findable", doi: ["10.1234/fake-doi"]) }
+
+  before { allow(Hyrax::DOI::RegisterDOIJob).to receive(:perform_now).with(any_args) }
+
+  describe '#delist_doi' do
+    it "changes the work DOI's status to registered" do
+      expect(Hyrax::DOI::RegisterDOIJob).to receive(:perform_now)
+                                                .with(work,
+                                                      registrar: work.doi_registrar.presence,
+                                                      registrar_opts: work.doi_registrar_opts)
+      work.delist_doi
+      expect(work.reload.doi_status_when_public).to eq "registered"
+    end
+  end
+
+  describe '#destroy' do
+
+    context 'when work meets all conditions for delisting doi' do
+      it 'delists the DOI' do
+        expect(work).to receive(:delist_doi)
+        work.destroy!
+      end
+    end
+
+    context 'when work does not meet conditions' do
+      before { work.visibility = "restricted" }
+
+      it 'does not try to delist the DOI' do
+        expect(work).not_to receive(:doi_status_when_public=)
+        expect(work).not_to receive(:save!)
+        expect(Hyrax::DOI::RegisterDOIJob).not_to receive(:perform_now)
+                                                  .with(work,
+                                                        registrar: work.doi_registrar.presence,
+                                                        registrar_opts: work.doi_registrar_opts)
+        work.destroy!
+      end
+    end
+
+  end
 end
