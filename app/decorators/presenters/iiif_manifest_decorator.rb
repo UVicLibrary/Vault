7bc4bb32f675b621a78902a78ed820273f47d3fc
Tiffany Chan <tjychan@uvic.ca>
2024-07-08 11:58:48 -0700

    Delete overrided method in IiifManifestDecorator that was causing issues.

--- app/decorators/presenters/iiif_manifest_decorator.rb
+++ app/decorators/presenters/iiif_manifest_decorator.rb
@@ -23,16 +23,4 @@ Hyrax::IiifManifestPresenter::DisplayImagePresenter.class_eval do
         Hyrax.config.iiif_metadata_fields
   end
 
-  def display_image
-    return nil unless solr_document.image?
-    return nil unless latest_file_id
-
-    IIIFManifest::DisplayImage
-      .new(display_image_url(hostname),
-      format: image_format(alpha_channels),
-      width: width,
-      height: height,
-      iiif_endpoint: iiif_endpoint(latest_file_id))
-  end
-
 end
