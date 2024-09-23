31956134f54d408f9c8b1184a05139904c275ed8
Tiffany Chan <tjychan@uvic.ca>
2024-09-23 14:28:51 -0700

    Update custom_range_limit_builder_spec.rb

--- spec/search_builders/custom_range_limit_builder_spec.rb
+++ spec/search_builders/custom_range_limit_builder_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe CustomRangeLimitBuilder do
   let(:ability) { double('ability') }
   let(:context) { FakeSearchBuilderScope.new }
   let(:user) { double('user') }
+  let(:solr_params) { { fq: [] } }
 
   subject { described_class.new(context) }
 
@@ -28,6 +29,28 @@ RSpec.describe CustomRangeLimitBuilder do
 
       it "excludes file sets" do
         expect(subject.models).not_to include(FileSet)
+      end
+    end
+  end
+
+  describe "#filter_models" do
+    context "with file set params" do
+      let(:solr_params) { { fq: ["{!term f=has_model_ssim}FileSet"] } }
+
+      it "replaces {!terms f=has_model_ssim}FileSet with all models" do
+        expect(subject.filter_models(solr_params).first).to include("Work")
+        expect(subject.filter_models(solr_params).first).to include("Collection")
+        expect(subject.filter_models(solr_params)).not_to include("{!terms f=has_model_ssim}FileSet")
+      end
+    end
+
+    context "without file set params" do
+      let(:file_set_params) { { "f" => { "keyword_tesim" => "keyword" } } }
+
+      it "excludes file sets" do
+        expect(subject.filter_models(solr_params).first).to include("Work")
+        expect(subject.filter_models(solr_params).first).to include("Collection")
+        expect(subject.filter_models(solr_params).first).not_to include("FileSet")
       end
     end
   end
