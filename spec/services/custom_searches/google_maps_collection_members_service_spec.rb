0bb17d1ffc4378e62396b1e0d4191b0fca62d29d
Tiffany Chan <tjychan@uvic.ca>
2024-10-16 13:08:43 -0700

    VAULT-193 Move Google Maps API key into .env and create a spec for GoogleMapsCollectionMembersService.

---
+++ spec/services/custom_searches/google_maps_collection_members_service_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe GoogleMapCollectionMembersService do
+
+  subject(:builder) do
+    described_class.new(scope: scope,
+                        collection: parent_collection,
+                        params: params)
+  end
+
+  let(:params) { ActionController::Parameters.new(id: parent_collection.id) }
+
+  let(:current_ability) { instance_double(Ability, admin?: true) }
+  let(:scope) { FakeSearchBuilderScope.new(current_ability: current_ability) }
+  let!(:subcollection) { create(:public_collection_lw, member_of_collections: [parent_collection], collection_type_settings: [:nestable]) }
+
+  let!(:parent_collection) { create(:public_collection_lw, collection_type_settings: [:nestable]) }
+
+  let!(:work1) { create(:generic_work, coordinates: ["52.8577, -2.03686"], title: ["Title A"], member_of_collections: [parent_collection]) }
+  let!(:work2) { create(:generic_work, title: ["Title B"], member_of_collections: [parent_collection]) }
+  let!(:work3) { create(:generic_work, coordinates: ["53.92527, -3.01085"], title: ["Title C"], member_of_collections: [subcollection]) }
+  let!(:work4) { create(:generic_work) }
+
+  describe '#available_member_works' do
+    it 'returns only nested works with coordinates' do
+      ids = builder.available_member_works.response[:docs].map { |work| work[:id] }
+      expect(ids).to contain_exactly(work1.id, work3.id)
+    end
+  end
+
+end
