7b256ba230c2975dd8b1bbb41457105f841eb4a1
Tiffany Chan <tjychan@uvic.ca>
2025-01-09 17:01:45 -0800

    Render all available facet items on advanced search page (advanced_search#index).

--- app/search_builders/custom_range_limit_builder.rb
+++ app/search_builders/custom_range_limit_builder.rb
@@ -8,43 +8,6 @@ class CustomRangeLimitBuilder < Hyrax::CatalogSearchBuilder
 
   self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
 
-  # Method added to to fetch proper things for date ranges.
-  def add_range_limit_params(solr_params)
-    super.tap do |solr_params| # solr_params is the original result from BlacklightRangeLimit::RangeLimitBuilder
-      # Optional: exclude Collections from results
-      # if Account.find_by(tenant: Apartment::Tenant.current).name == "vault"
-      #   solr_params[:fq] ||= []
-      #   solr_params[:fq] << "has_model_ssim: GenericWork"
-      # else
-      #   solr_params[:fq] ||= []
-      #   solr_params[:fq] << "has_model_ssim: IaffWork"
-      # end
-    end
-  end
-
-  # Another processing method, this one is NOT included in default processing chain,
-  # it is specifically swapped in *instead of* add_range_limit_params for
-  # certain ajax requests that only want to fetch range limit segments for
-  # ONE field.
-  #
-  # It turns off faceting and sets rows to 0 as well, only results for
-  # single specified field are needed.
-  #
-  # Specified field and parameters are specified in incoming parameters
-  # range_field, range_start, range_end
-  def fetch_specific_range_limit(solr_params)
-    super.tap do |solr_params| # solr_params is the original result from BlacklightRangeLimit::RangeLimitBuilder
-      # Optional: exclude admin sets from results
-      # if Account.find_by(tenant: Apartment::Tenant.current).name == "vault"
-      #   solr_params[:fq] ||= []
-      #   solr_params[:fq] << "has_model_ssim: GenericWork"
-      # else
-      #   solr_params[:fq] ||= []
-      #   solr_params[:fq] << "has_model_ssim: IaffWork"
-      # end
-    end
-  end
-
   # If a user enters a search query but no sort, sort the results
   # by relevance (instead of "year_sort_dtsi title_sort_ssi asc",
   # which is used as the default for displaying works)
@@ -58,23 +21,18 @@ class CustomRangeLimitBuilder < Hyrax::CatalogSearchBuilder
     end
   end
 
-  # A Solr param filter that is NOT included by default in the chain,
-  # but is appended by AdvancedController#index, to do a search
-  # for facets _ignoring_ the current query, we want the facets
-  # as if the current query weren't there.
-  #
-  # Also adds any solr params set in blacklight_config.advanced_search[:form_solr_parameters]
+  # OVERRIDE Blacklight Advanced Search v.7
+  # Delete facet field-specific limits that were overriding the
+  # configured advanced search facet limit (-1)
   def facets_for_advanced_search_form(solr_params)
-    # ensure empty query is all records, to fetch available facets on entire corpus
-    solr_params["q"]            = '{!lucene}*:*'
-    # explicitly use lucene defType since we are passing a lucene query above (and appears to be required for solr 7)
-    solr_params["defType"]      = 'lucene'
-    # We only care about facets, we don't need any rows.
-    solr_params["rows"]         = "0"
+    super.tap do |solr_params|
+      advanced_search_facet_config = blacklight_config.advanced_search[:form_solr_parameters]
 
-    # Anything set in config as a literal
-    if blacklight_config.advanced_search[:form_solr_parameters]
-      solr_params.merge!(blacklight_config.advanced_search[:form_solr_parameters])
+      if blacklight_config.advanced_search[:form_solr_parameters] && advanced_search_facet_config.has_key?('facet.limit')
+        solr_params['facet.field'].each do |field_key|
+          solr_params.delete("f.#{field_key}.facet.limit")
+        end
+      end
     end
   end
 
