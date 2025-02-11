987127c0ace24bf5efcde0f78bb1d1525fa0d566
Tiffany Chan <tjychan@uvic.ca>
2025-02-11 11:11:05 -0800

    VAULT-198 Delete some unnecessary presenter files and update VaultCollectionPresenter for Bootstrap/Hyrax 4.

--- app/presenters/hyrax/image_presenter.rb
+++
@@ -1,8 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Image`
-module Hyrax
-  class ImagePresenter < Hyrax::WorkShowPresenter
-    # We do not use this generated ImagePresenter. Instead we use the
-    # Hyrax::WorkShowPresenter
-  end
-end
