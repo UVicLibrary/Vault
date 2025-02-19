b1cbc1c6ddb3bdb0e494ea57783b85ed3af09785
Tiffany Chan <tjychan@uvic.ca>
2025-02-19 11:28:14 -0800

    VAULT-218 Add rows parameter to query in ReindexNestedMembersJob.

--- app/jobs/reindex_nested_members_job.rb
+++ app/jobs/reindex_nested_members_job.rb
@@ -3,7 +3,7 @@ class ReindexNestedMembersJob < ActiveJob::Base
   # @param [String] - the parent collection id
   def perform(collection_id)
     query = NestedWorksSearchService.nested_members_query(collection_id)
-    Hyrax::SolrService.get(query, fl: 'id')['response']['docs'].map do |doc|
+    Hyrax::SolrService.get(query, fl: 'id', rows: 5000)['response']['docs'].map do |doc|
       ReindexObjectJob.perform_later(doc['id'])
     end
   end
