03dc1d900740512e580750106e29f741f2e7750f
Tiffany Chan <tjychan@uvic.ca>
2025-02-28 17:27:34 -0800

    Fix failing file_sets_controller_spec.

--- spec/controllers/hyrax/file_sets_controller_spec.rb
+++ spec/controllers/hyrax/file_sets_controller_spec.rb
@@ -132,7 +132,7 @@ RSpec.describe Hyrax::FileSetsController do
         it "spawns a ContentNewVersionEventJob" do
           expect(ContentNewVersionEventJob).to receive(:perform_later).with(file_set, user)
           expect(actor).to receive(:ingest_file).with(JobIoWrapper).and_return(true)
-          file = fixture_file_upload('/world.png', 'image/png')
+          file = fixture_file_upload('world.png', 'image/png')
           post :update, params: { id: file_set, filedata: file, file_set: { keyword: [''], permissions_attributes: [{ type: 'person', name: 'archivist1', access: 'edit' }] } }
           post :update, params: { id: file_set, file_set: { files: [file], keyword: [''], permissions_attributes: [{ type: 'person', name: 'archivist1', access: 'edit' }] } }
         end
@@ -155,7 +155,7 @@ RSpec.describe Hyrax::FileSetsController do
           expect(ContentNewVersionEventJob)
             .to receive(:perform_later)
             .with(file_set, user)
-          file = fixture_file_upload('/world.png', 'image/png')
+          file = fixture_file_upload('world.png', 'image/png')
           allow(Hyrax::UploadedFile)
             .to receive(:find)
             .with(["1"])
