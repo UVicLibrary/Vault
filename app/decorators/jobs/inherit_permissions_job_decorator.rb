7d696373eb6d6bf5a7ee14abcd5163910b73b752
Tiffany Chan <tjychan@uvic.ca>
2025-03-18 13:30:55 -0700

    Resave the work if InheritPermissionsJob fails (fixes bug where works appear private even after being set to public).

---
+++ app/decorators/jobs/inherit_permissions_job_decorator.rb
@@ -0,0 +1,16 @@
+InheritPermissionsJob.class_eval do
+
+  # Intermittent error where job that fails on 1st try (but later
+  # succeeds) causes work to appear private, even when it has
+  # been set to public. Resaving the work solves this issue.
+  rescue_from(ActiveFedora::ModelMismatch) do |_exception|
+    work = arguments[0]
+    case work
+    when ActiveFedora::Base
+      work.save!
+    else
+      Hyrax.persister.save(resource: work)
+    end
+  end
+
+end
