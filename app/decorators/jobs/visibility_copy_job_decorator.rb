1c82af7120d7fa694026f903d2813861f1166603
Tiffany Chan <sufia@himalia.library.uvic.ca>
2025-04-14 11:38:33 -0700

    Rescue inconsistent ActiveFedora::ModelMismatch error in VisibilityCopyJob.

---
+++ app/decorators/jobs/visibility_copy_job_decorator.rb
@@ -0,0 +1,25 @@
+VisibilityCopyJob.class_eval do
+
+  # Intermittent error where job that fails on 1st try (but later
+  # succeeds) causes work to appear private, even when it has
+  # been set to public. Resaving the work solves this issue.
+  rescue_from(ActiveFedora::ModelMismatch) do |_exception|
+    work = arguments[0]
+    case work
+    when ActiveFedora::Base
+      work.save! && work.update_index
+    else
+      Hyrax.persister.save(resource: work)
+    end
+    retry_job
+  end
+
+  after_perform do |job|
+    work = job.arguments.first
+    case work
+    when ActiveFedora::Base
+      work.update_index
+    end
+  end
+
+end
